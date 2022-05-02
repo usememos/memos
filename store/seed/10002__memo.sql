8b209924244061c656e431ac18091497860aade9
boojack <imrealleonardo@gmail.com>
2022-05-02 10:57:31 +0800

    chore: update seed data

--- store/seed/10002__memo.sql
+++ store/seed/10002__memo.sql
@@ -8,3 +8,16 @@ VALUES
     '#memos 👋 Welcome to memos', 
     101
   );
+
+INSERT INTO 
+  memo (
+    `content`, 
+    `creator_id`,
+    `row_status`
+  )
+VALUES
+  (
+    '好好学习，天天向上。', 
+    101,
+    'ARCHIVED'
+  );
