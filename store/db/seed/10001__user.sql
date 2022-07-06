a5c37ff6fdfccb90a2fd85662f9f7b303e6c080d
Hyoban <hi@hyoban.cc>
2022-07-06 19:22:05 +0800

    chore: add a normal user to seed

--- store/db/seed/10001__user.sql
+++ store/db/seed/10001__user.sql
@@ -17,3 +17,23 @@ VALUES
     -- raw password: secret
     '$2a$14$ajq8Q7fbtFRQvXpdCq7Jcuy.Rx1h/L4J60Otx.gyNLbAYctGMJ9tK'
   );
+
+INSERT INTO 
+  user (
+    `id`, 
+    `email`,
+    `role`,
+    `name`, 
+    `open_id`,
+    `password_hash`
+  )
+VALUES
+  (
+    102, 
+    'demo2@usememos.com',
+    'USER',
+    'Demo User',
+    'demo_open_id2',
+    -- raw password: secret
+    '$2a$14$ajq8Q7fbtFRQvXpdCq7Jcuy.Rx1h/L4J60Otx.gyNLbAYctGMJ9tK'
+  );
