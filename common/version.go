91d45d6d461fab2126b63d646a6be08ff5273a2a
boojack <imrealleonardo@gmail.com>
2022-07-09 14:09:40 +0800

    chore: release `v0.2.0` (#114)

--- common/version.go
+++ common/version.go
@@ -7,7 +7,7 @@ import (
 
 // Version is the service current released version.
 // Semantic versioning: https://semver.org/
-var Version = "0.1.3"
+var Version = "0.2.0"
 
 // DevVersion is the service current development version.
 var DevVersion = "0.2.0"
