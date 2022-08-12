dfac877957f000cfbaf517546a01489199640655
XQ <qiaobingxue1998@163.com>
2022-08-12 21:30:59 +0800

    feat: add `manifest.json` for pwa (#144)
    * chore: update `i18nStore`
    
    * feat: add pwa `manifest.json`

---
+++ web/public/sw.js
@@ -0,0 +1,10 @@
+self.addEventListener("install", (event) => {
+    event.waitUntil((async () => {})());
+  });
+  
+  self.addEventListener("activate", (event) => {
+    event.waitUntil((async () => {})());
+  });
+  
+  self.addEventListener("fetch", (event) => {});
+  
