2cf07753e7a68adf81b2a056607c0bb4315f5571
Steven <stevenlgtm@gmail.com>
2022-08-20 11:37:19 +0800

    chore: update version `0.4.0`

--- common/version.go
+++ common/version.go
@@ -7,10 +7,10 @@ import (
 
 // Version is the service current released version.
 // Semantic versioning: https://semver.org/
-var Version = "0.3.1"
+var Version = "0.4.0"
 
 // DevVersion is the service current development version.
-var DevVersion = "0.3.1"
+var DevVersion = "0.4.0"
 
 func GetCurrentVersion(mode string) string {
 	if mode == "dev" {
