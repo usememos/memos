d5655ab929b0658d5b9e03a028ac7f98eb6cabe8
Steven <stevenlgtm@gmail.com>
2022-08-25 19:21:19 +0800

    fix: tag regexp

--- server/tag.go
+++ server/tag.go
@@ -47,7 +47,7 @@ func (s *Server) registerTagRoutes(g *echo.Group) {
 
 		tagMapSet := make(map[string]bool)
 
-		r := regexp.MustCompile("#(.+?) ")
+		r := regexp.MustCompile(`#([^\s]+?) `)
 		if err != nil {
 			return echo.NewHTTPError(http.StatusInternalServerError, "Failed to compile regexp").SetInternal(err)
 		}
