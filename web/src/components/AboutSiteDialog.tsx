d3b26f71261881ca57e9375e5d752ee213811fa8
Steven <stevenlgtm@gmail.com>
2022-09-02 20:12:13 +0800

    chore: update about dialog

--- web/src/components/AboutSiteDialog.tsx
+++ web/src/components/AboutSiteDialog.tsx
@@ -38,13 +38,14 @@ const AboutSiteDialog: React.FC<Props> = ({ destroy }: Props) => {
       <div className="dialog-header-container">
         <p className="title-text">
           <span className="icon-text">🤠</span>
-          {t("common.about")} <b>Memos</b>
+          {t("common.about")}
         </p>
         <button className="btn close-btn" onClick={handleCloseBtnClick}>
           <Icon.X />
         </button>
       </div>
       <div className="dialog-content-container">
+        <img className="logo-img" src="/logo-full.webp" alt="" />
         <p>{t("slogan")}</p>
         <br />
         <div className="addtion-info-container">
