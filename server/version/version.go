721aa3c9072f0aefa3d8042836ee1be16c01f05c
Steven <stevenlgtm@gmail.com>
2022-09-16 23:04:40 +0800

    chore: update `v0.4.4`

--- server/version/version.go
+++ server/version/version.go
@@ -7,7 +7,7 @@ import (
 
 // Version is the service current released version.
 // Semantic versioning: https://semver.org/
-var Version = "0.4.3"
+var Version = "0.4.4"
 
 // DevVersion is the service current development version.
 var DevVersion = "0.4.4"
