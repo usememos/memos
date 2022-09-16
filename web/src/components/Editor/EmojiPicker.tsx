e7db587a9e3da7799842356b972d9a8078ee6bf7
f97 <huynhduckhoan@gmail.com>
2022-09-16 23:42:52 +0700

    feat: add emoji picker in editor (#221)
    * feat: add vietnamese
    
    * feat: add emoji picker in editor
    
    * fix failing checks
    
    * move emoji button before upload button
    
    * move script to body index.html
    
    * Update web/index.html
    
    Co-authored-by: boojack <stevenlgtm@gmail.com>

---
+++ web/src/components/Editor/EmojiPicker.tsx
@@ -0,0 +1,41 @@
+import { forwardRef, useEffect } from "react";
+import Picker, { IEmojiPickerProps } from "emoji-picker-react";
+
+export type EmojiPickerElement = HTMLDivElement;
+
+interface Props {
+  isShowEmojiPicker: boolean;
+  onEmojiClick: IEmojiPickerProps["onEmojiClick"];
+  handleChangeIsShowEmojiPicker: (status: boolean) => void;
+}
+
+export const EmojiPicker = forwardRef<EmojiPickerElement, Props>((props: Props, ref) => {
+  const { isShowEmojiPicker, onEmojiClick, handleChangeIsShowEmojiPicker } = props;
+
+  useEffect(() => {
+    if (isShowEmojiPicker) {
+      const handleClickOutside = (event: MouseEvent) => {
+        const emojiWrapper = document.querySelector(".emoji-picker-react");
+        const isContains = emojiWrapper?.contains(event.target as Node);
+        if (!isContains) {
+          handleChangeIsShowEmojiPicker(false);
+        }
+      };
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        // Unbind the event listener on clean up
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }
+  }, [isShowEmojiPicker]);
+
+  return (
+    <div className="emoji-picker" ref={ref}>
+      <Picker onEmojiClick={onEmojiClick} />
+    </div>
+  );
+});
+
+EmojiPicker.displayName = "EmojiPicker";
+
+export default EmojiPicker;
