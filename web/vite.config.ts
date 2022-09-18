218ebe3e5bec6ff990484feda3edeabc252f9088
Steven <stevenlgtm@gmail.com>
2022-09-18 11:47:27 +0800

    chore: add `vite-plugin-pwa`

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA()],
   server: {
     host: "0.0.0.0",
     port: 3000,
@@ -21,11 +21,6 @@ export default defineConfig({
         target: "http://localhost:8080/",
         changeOrigin: true,
       },
-    },
-  },
-  resolve: {
-    alias: {
-      "@/": `${resolve(__dirname, "src")}/`,
     },
   },
 });
