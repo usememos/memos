a2180f177fdd2e0637cb9ffa04656c8e4275131d
Steven <stevenlgtm@gmail.com>
2022-09-26 21:40:37 +0800

    chore: remove `vite-plugin-pwa`

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA()],
+  plugins: [react()],
   server: {
     host: "0.0.0.0",
     port: 3000,
