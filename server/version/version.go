d670adc11f2ae565d568ceb5cafb5c14828bfc51
Steven <stevenlgtm@gmail.com>
2022-10-14 23:25:07 +0800

    chore: release `v0.6.0`

--- server/version/version.go
+++ server/version/version.go
@@ -7,7 +7,7 @@ import (
 
 // Version is the service current released version.
 // Semantic versioning: https://semver.org/
-var Version = "0.5.0"
+var Version = "0.6.0"
 
 // DevVersion is the service current development version.
 var DevVersion = "0.6.0"
