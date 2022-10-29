56123e9b1427f2607b00cc84f2ed676fe2230c11
Steven <stevenlgtm@gmail.com>
2022-10-29 18:13:29 +0800

    feat: add blockquote regexp

---
+++ web/src/labs/marked/parser/Blockquote.ts
@@ -0,0 +1,18 @@
+import { escape } from "lodash";
+
+export const BLOCKQUOTE_REG = /> ([\S ]+)(\n?)/;
+
+const renderer = (rawStr: string): string => {
+  const matchResult = rawStr.match(BLOCKQUOTE_REG);
+  if (!matchResult) {
+    return rawStr;
+  }
+
+  return `<blockquote>${escape(matchResult[1])}</blockquote>${matchResult[2]}`;
+};
+
+export default {
+  name: "blockqoute",
+  regex: BLOCKQUOTE_REG,
+  renderer,
+};
