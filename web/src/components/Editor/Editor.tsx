98b2e9333031b33621e685bbca70038330e10cb6
Steven <stevenlgtm@gmail.com>
2022-11-11 08:03:36 +0800

    feat: update visibility selector style

--- web/src/components/Editor/Editor.tsx
+++ web/src/components/Editor/Editor.tsx
@@ -17,14 +17,12 @@ interface Props {
   placeholder: string;
   fullscreen: boolean;
   tools?: ReactNode;
-  onPaste: (event: React.ClipboardEvent) => void;
-  onFocus: () => void;
   onContentChange: (content: string) => void;
+  onPaste: (event: React.ClipboardEvent) => void;
 }
 
-// eslint-disable-next-line react/display-name
-const Editor = forwardRef((props: Props, ref: React.ForwardedRef<EditorRefActions>) => {
-  const { className, initialContent, placeholder, fullscreen, onPaste, onFocus, onContentChange: handleContentChangeCallback } = props;
+const Editor = forwardRef(function Editor(props: Props, ref: React.ForwardedRef<EditorRefActions>) {
+  const { className, initialContent, placeholder, fullscreen, onPaste, onContentChange: handleContentChangeCallback } = props;
   const editorRef = useRef<HTMLTextAreaElement>(null);
   const refresh = useRefresh();
 
@@ -106,7 +104,6 @@ const Editor = forwardRef((props: Props, ref: React.ForwardedRef<EditorRefAction
         ref={editorRef}
         onPaste={onPaste}
         onInput={handleEditorInput}
-        onFocus={onFocus}
       ></textarea>
     </div>
   );
