8cc0977a01c87916934eb748ccee75f25a29c26d
Zeng1998 <1129142694@qq.com>
2022-11-12 21:25:50 +0800

    fix: image url extraction (#453)
    fix: image-url-extraction
    
    Co-authored-by: boojack <stevenlgtm@gmail.com>

--- web/src/components/Memo.tsx
+++ web/src/components/Memo.tsx
@@ -165,7 +165,14 @@ const Memo: React.FC<Props> = (props: Props) => {
 
       if (currImgUrl) {
         // use regex to get all image urls from memo content
-        const imageUrls = memo.content.match(/!\[.*?\]\((.*?)\)/g)?.map((item) => item.match(/\((.*?)\)/)?.[1] ?? "") ?? [];
+        const imageUrls =
+          memo.content.match(/!\[.*?\]\((.*?)\)/g)?.map(
+            (item) =>
+              item
+                .match(/\((.*?)\)/g)
+                ?.slice(-1)[0]
+                .slice(1, -1) ?? ""
+          ) ?? [];
         showPreviewImageDialog(
           imageUrls,
           imageUrls.findIndex((item) => item === currImgUrl)
