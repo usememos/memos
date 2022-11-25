037649d052ebfa45eac6dd319e1c67eeb5df07f5
Steven <stevenlgtm@gmail.com>
2022-11-25 22:13:38 +0800

    feat: update prod version

--- store/db/migration/dev/LATEST__SCHEMA.sql
+++ store/db/migration/dev/LATEST__SCHEMA.sql
@@ -86,15 +86,3 @@ CREATE TABLE memo_resource (
   updated_ts BIGINT NOT NULL DEFAULT (strftime('%s', 'now')),
   UNIQUE(memo_id, resource_id)
 );
-
--- memo_relation
-CREATE TABLE memo_relation (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  created_ts BIGINT NOT NULL DEFAULT (strftime('%s', 'now')),
-  updated_ts BIGINT NOT NULL DEFAULT (strftime('%s', 'now')),
-  memo_id INTEGER NOT NULL,
-  related_memo_id INTEGER NOT NULL,
-  type TEXT NOT NULL,
-  description TEXT NOT NULL DEFAULT '',
-  UNIQUE(memo_id, related_memo_id, type)
-);
