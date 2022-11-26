6e5537d131e1e1b4e9dc2b33c7a54342d86725e0
Stephen Zhou <hi@hyoban.cc>
2022-11-26 12:19:00 +0800

    feat: dark mode support for explore page (#584)
    * feat: dark mode support for auth page
    
    * chore: update
    
    * feat: dark mode support for explore page (#583)
    
    * fix: avoid white text
    
    * fix: import order

--- web/src/pages/Explore.tsx
+++ web/src/pages/Explore.tsx
@@ -9,6 +9,7 @@ import useLoading from "../hooks/useLoading";
 import toastHelper from "../components/Toast";
 import MemoContent from "../components/MemoContent";
 import MemoResources from "../components/MemoResources";
+import useApperance from "../hooks/useApperance";
 import "../less/explore.less";
 
 interface State {
@@ -16,6 +17,8 @@ interface State {
 }
 
 const Explore = () => {
+  useApperance();
+
   const { t, i18n } = useTranslation();
   const user = useAppSelector((state) => state.user.user);
   const location = useAppSelector((state) => state.location);
