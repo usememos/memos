9b8d266ee3df32000fa3696b3b0ff343af19d47a
wqh1996 <wqh.1996@gmail.com>
2022-11-27 12:47:32 +0000

    fix: shoutcut tag filter handle mutiple tags

--- web/src/labs/marked/parser/Tag.ts
+++ web/src/labs/marked/parser/Tag.ts
@@ -1,14 +1,15 @@
 import { escape } from "lodash-es";
 
-export const TAG_REG = /#([^\s#]+?) /;
+export const TAG_REG = /#([^\s#]+?) /g;
 
 const renderer = (rawStr: string): string => {
   const matchResult = rawStr.match(TAG_REG);
   if (!matchResult) {
     return rawStr;
   }
+  const tag = matchResult[0].replace(TAG_REG, "$1").trim();
 
-  return `<span class='tag-span'>#${escape(matchResult[1])}</span> `;
+  return `<span class='tag-span'>#${escape(tag)}</span> `;
 };
 
 export default {
