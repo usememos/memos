c95a5b5541dab889b57864f0539a3c4c6dd7c5e4
wqh1996 <wqh.1996@gmail.com>
2022-11-28 02:51:39 +0000

    not edit parser

--- web/src/labs/marked/parser/Tag.ts
+++ web/src/labs/marked/parser/Tag.ts
@@ -1,15 +1,14 @@
 import { escape } from "lodash-es";
 
-export const TAG_REG = /#([^\s#]+?) /g;
+export const TAG_REG = /#([^\s#]+?) /;
 
 const renderer = (rawStr: string): string => {
   const matchResult = rawStr.match(TAG_REG);
   if (!matchResult) {
     return rawStr;
   }
-  const tag = matchResult[0].replace(TAG_REG, "$1").trim();
 
-  return `<span class='tag-span'>#${escape(tag)}</span> `;
+  return `<span class='tag-span'>#${escape(matchResult[1])}</span> `;
 };
 
 export default {
