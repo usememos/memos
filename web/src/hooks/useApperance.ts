915d76d8f0a937f5441cde111612a5cc0e32f8c2
hyoban <hi@hyoban.cc>
2022-12-01 15:12:50 +0800

    fix: apperance can not auto switch

--- web/src/hooks/useApperance.ts
+++ web/src/hooks/useApperance.ts
@@ -3,24 +3,26 @@ import { useColorScheme } from "@mui/joy/styles";
 
 import { APPERANCE_OPTIONS, APPERANCE_OPTIONS_STORAGE_KEY } from "../helpers/consts";
 import useLocalStorage from "./useLocalStorage";
+import useMediaQuery from "./useMediaQuery";
 
 export type Apperance = typeof APPERANCE_OPTIONS[number];
 
 const useApperance = () => {
   const [apperance, setApperance] = useLocalStorage<Apperance>(APPERANCE_OPTIONS_STORAGE_KEY, APPERANCE_OPTIONS[0]);
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
 
   const { setMode } = useColorScheme();
 
   useEffect(() => {
     const root = document.documentElement;
-    if (apperance === "dark" || (apperance === "auto" && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+    if (apperance === "dark" || (apperance === "auto" && prefersDarkMode)) {
       root.classList.add("dark");
       setMode("dark");
     } else {
       root.classList.remove("dark");
       setMode("light");
     }
-  }, [apperance]);
+  }, [apperance, prefersDarkMode]);
 
   return [apperance, setApperance] as const;
 };
