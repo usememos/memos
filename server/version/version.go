dd6e2337e6ba6665adf9a27672fa8352594ee1a1
boojack <stevenlgtm@gmail.com>
2022-12-10 13:23:38 +0800

    chore: update version to `0.8.2` (#722)

--- server/version/version.go
+++ server/version/version.go
@@ -7,10 +7,10 @@ import (
 
 // Version is the service current released version.
 // Semantic versioning: https://semver.org/
-var Version = "0.8.1"
+var Version = "0.8.2"
 
 // DevVersion is the service current development version.
-var DevVersion = "0.8.1"
+var DevVersion = "0.8.2"
 
 func GetCurrentVersion(mode string) string {
 	if mode == "dev" {
