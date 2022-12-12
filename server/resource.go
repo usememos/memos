4bb972dfd91fabfaa59ff5db93dd23e4a7cd3df7
Zeng1998 <1129142694@qq.com>
2022-12-12 19:57:00 +0800

    update
    Co-authored-by: boojack <stevenlgtm@gmail.com>

--- server/resource.go
+++ server/resource.go
@@ -249,7 +249,7 @@ func (s *Server) registerResourcePublicRoutes(g *echo.Group) {
 		if err != nil {
 			return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("ID is not a number: %s", c.Param("resourceId"))).SetInternal(err)
 		}
-		filename, _ := url.QueryUnescape(c.Param("filename"))
+		filename, err := url.QueryUnescape(c.Param("filename"))
 		if err != nil {
 			return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("filename is invalid: %s", c.Param("filename"))).SetInternal(err)
 		}
