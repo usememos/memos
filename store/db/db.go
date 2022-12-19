47cdc860488e637d171731d001f29b3f9073769c
Steven <stevenlgtm@gmail.com>
2022-12-19 18:43:10 +0800

    chore: return raw text for html

--- store/db/db.go
+++ store/db/db.go
@@ -43,11 +43,10 @@ func (db *DB) Open(ctx context.Context) (err error) {
 	}
 
 	// Connect to the database without foreign_key.
-	sqliteDB, err := sql.Open("sqlite3", db.profile.DSN+"?_foreign_keys=0")
+	sqliteDB, err := sql.Open("sqlite3", db.profile.DSN+"?cache=shared&_foreign_keys=0&_busy_timeout=9999999")
 	if err != nil {
 		return fmt.Errorf("failed to open db with dsn: %s, err: %w", db.profile.DSN, err)
 	}
-	sqliteDB.SetMaxOpenConns(1)
 	db.Db = sqliteDB
 
 	if db.profile.Mode == "dev" {
