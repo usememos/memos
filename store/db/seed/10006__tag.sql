5c956e1674ce4a6b5edf52652fab5dd3bb39e2a0
Steven <stevenlgtm@gmail.com>
2022-12-22 08:30:53 +0800

    chore: update seed data

---
+++ store/db/seed/10006__tag.sql
@@ -0,0 +1,32 @@
+INSERT INTO 
+  tag (
+    `name`, 
+    `creator_id`
+  )
+VALUES
+  (
+    'Hello', 
+    101
+  );
+
+INSERT INTO 
+  tag (
+    `name`, 
+    `creator_id`
+  )
+VALUES
+  (
+    'TODO', 
+    101
+  );
+
+INSERT INTO 
+  tag (
+    `name`, 
+    `creator_id`
+  )
+VALUES
+  (
+    'TODO', 
+    102
+  );
