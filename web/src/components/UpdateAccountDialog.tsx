8d3da288af0686ab3338ca0995bcf2d9b624a27d
Vincenzo Cardone <75483183+Block2Paz@users.noreply.github.com>
2023-01-02 00:10:23 +0100

    Update UpdateAccountDialog.tsx

--- web/src/components/UpdateAccountDialog.tsx
+++ web/src/components/UpdateAccountDialog.tsx
@@ -111,7 +111,7 @@ const UpdateAccountDialog: React.FC<Props> = ({ destroy }: Props) => {
       <div className="dialog-content-container">
         <p className="text-sm mb-1">
           {t("common.nickname")}
-		  <span className="text-sm text-gray-400 ml-1">(Display in the banner)</span>
+          <span className="text-sm text-gray-400 ml-1">(Display in the banner)</span>
         </p>
         <input type="text" className="input-text" value={state.nickname} onChange={handleNicknameChanged} />
         <p className="text-sm mb-1 mt-2">
@@ -121,7 +121,7 @@ const UpdateAccountDialog: React.FC<Props> = ({ destroy }: Props) => {
         <input type="text" className="input-text" value={state.username} onChange={handleUsernameChanged} />
         <p className="text-sm mb-1 mt-2">
           {t("common.email")}
-		  <span className="text-sm text-gray-400 ml-1">(Optional)</span>
+          <span className="text-sm text-gray-400 ml-1">(Optional)</span>
         </p>
         <input type="text" className="input-text" value={state.email} onChange={handleEmailChanged} />
         <div className="mt-4 w-full flex flex-row justify-end items-center space-x-2">
