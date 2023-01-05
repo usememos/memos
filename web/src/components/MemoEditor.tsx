ea85251805c6c44ff866be7ea304d6c716f27bc0
hyoban <hi@hyoban.cc>
2023-01-05 17:35:57 +0800

    feat: scrool to memo after editing

--- web/src/components/MemoEditor.tsx
+++ web/src/components/MemoEditor.tsx
@@ -295,6 +295,15 @@ const MemoEditor = () => {
     return resource;
   };
 
+  const scrollToEditingMemo = useCallback(() => {
+    if (editorState.editMemoId) {
+      const memoElements = document.getElementsByClassName(`memos-${editorState.editMemoId}`);
+      if (memoElements.length !== 0) {
+        memoElements[0].scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, [editorState.editMemoId]);
+
   const handleSaveBtnClick = async () => {
     const content = editorRef.current?.getContent();
     if (!content) {
@@ -346,6 +355,8 @@ const MemoEditor = () => {
     setEditorContentCache("");
     storage.remove(["editingMemoVisibilityCache"]);
     editorRef.current?.setContent("");
+
+    scrollToEditingMemo();
   };
 
   const handleCancelEdit = () => {
@@ -356,6 +367,8 @@ const MemoEditor = () => {
       setEditorContentCache("");
       storage.remove(["editingMemoVisibilityCache"]);
     }
+
+    scrollToEditingMemo();
   };
 
   const handleContentChange = (content: string) => {
