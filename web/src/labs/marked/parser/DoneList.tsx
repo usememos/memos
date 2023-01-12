b19c3c6db3d88e5043aa8b1de2d8c7fa807e6561
boojack <stevenlgtm@gmail.com>
2023-01-12 08:52:57 +0800

    feat: update renderer in list (#935)

--- web/src/labs/marked/parser/DoneList.tsx
+++ web/src/labs/marked/parser/DoneList.tsx
@@ -2,17 +2,18 @@ import { inlineElementParserList } from ".";
 import { marked } from "..";
 import { matcher } from "../matcher";
 
-export const DONE_LIST_REG = /^- \[[xX]\] ([^\n]+)/;
+export const DONE_LIST_REG = /^( *)- \[[xX]\] ([^\n]+)/;
 
 const renderer = (rawStr: string) => {
   const matchResult = matcher(rawStr, DONE_LIST_REG);
   if (!matchResult) {
     return rawStr;
   }
-
-  const parsedContent = marked(matchResult[1], [], inlineElementParserList);
+  const space = matchResult[1];
+  const parsedContent = marked(matchResult[2], [], inlineElementParserList);
   return (
     <p className="li-container">
+      <span className="whitespace-pre">{space}</span>
       <span className="todo-block done" data-value="DONE">
         ✓
       </span>
--- web/src/labs/marked/parser/OrderedList.tsx
+++ web/src/labs/marked/parser/OrderedList.tsx
@@ -2,18 +2,19 @@ import { inlineElementParserList } from ".";
 import { marked } from "..";
 import { matcher } from "../matcher";
 
-export const ORDERED_LIST_REG = /^(\d+)\. (.+)/;
+export const ORDERED_LIST_REG = /^( *)(\d+)\. (.+)/;
 
 const renderer = (rawStr: string) => {
   const matchResult = matcher(rawStr, ORDERED_LIST_REG);
   if (!matchResult) {
     return rawStr;
   }
-
-  const parsedContent = marked(matchResult[2], [], inlineElementParserList);
+  const space = matchResult[1];
+  const parsedContent = marked(matchResult[3], [], inlineElementParserList);
   return (
     <p className="li-container">
-      <span className="ol-block">{matchResult[1]}.</span>
+      <span className="whitespace-pre">{space}</span>
+      <span className="ol-block">{matchResult[2]}.</span>
       <span>{parsedContent}</span>
     </p>
   );
