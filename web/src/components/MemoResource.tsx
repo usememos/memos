3ce52bf2fd838452daeb16ed23d8206b57c35d7b
Steven <stevenlgtm@gmail.com>
2023-02-12 16:32:37 +0800

    chore: simplify memo editor component

--- web/src/components/MemoResource.tsx
+++ web/src/components/MemoResource.tsx
@@ -16,7 +16,7 @@ const MemoResource: React.FC<Props> = (props: Props) => {
 
   return (
     <>
-      <div className={`w-auto flex flex-row justify-start items-center ${className}`}>
+      <div className={`w-auto flex flex-row justify-start items-center hover:opacity-80 ${className}`}>
         {resource.type.startsWith("audio") ? (
           <>
             <audio className="h-8" src={resourceUrl} controls></audio>
@@ -24,7 +24,7 @@ const MemoResource: React.FC<Props> = (props: Props) => {
         ) : (
           <>
             <Icon.FileText className="w-4 h-auto mr-1 text-gray-500" />
-            <span className="text-gray-500 text-sm max-w-xs truncate font-mono cursor-pointer" onClick={handlePreviewBtnClick}>
+            <span className="text-gray-500 text-sm max-w-[256px] truncate font-mono cursor-pointer" onClick={handlePreviewBtnClick}>
               {resource.filename}
             </span>
           </>
