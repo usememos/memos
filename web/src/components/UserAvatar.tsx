bcee0bbf3a8fcbc6c513671cb6b4d1cca38548e6
boojack <stevenlgtm@gmail.com>
2023-02-18 10:00:46 +0800

    feat: add avatar to user in frontend (#1108)

---
+++ web/src/components/UserAvatar.tsx
@@ -0,0 +1,17 @@
+import { MEMOS_LOGO_URL } from "../helpers/consts";
+
+interface Props {
+  avatarUrl?: string;
+  className?: string;
+}
+
+const UserAvatar = (props: Props) => {
+  const { avatarUrl, className } = props;
+  return (
+    <div className={`${className ?? ""} w-8 h-8 rounded-full bg-gray-100 dark:bg-zinc-800`}>
+      <img className="w-full h-full" src={avatarUrl || MEMOS_LOGO_URL} alt="" />
+    </div>
+  );
+};
+
+export default UserAvatar;
