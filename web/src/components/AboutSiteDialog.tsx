7b7061846c63d4e3377128e13a1d5a149e2c5eae
Zeng1998 <acan.coder@gmail.com>
2023-02-27 19:50:43 +0800

    chore: open url in other tabs (#1173)
    * chore: open url in other tabs
    
    * update: add `rel="noreferrer"`

--- web/src/components/AboutSiteDialog.tsx
+++ web/src/components/AboutSiteDialog.tsx
@@ -32,7 +32,12 @@ const AboutSiteDialog: React.FC<Props> = ({ destroy }: Props) => {
         <div className="mt-4 flex flex-row text-sm justify-start items-center">
           <div className="flex flex-row justify-start items-center mr-2">
             Powered by
-            <a href="https://usememos.com" className="flex flex-row justify-start items-center mr-1 hover:underline">
+            <a
+              href="https://usememos.com"
+              target="_blank"
+              rel="noreferrer"
+              className="flex flex-row justify-start items-center mr-1 hover:underline"
+            >
               <img className="w-6 h-auto" src="/logo.png" alt="" />
               memos
             </a>
@@ -42,7 +47,12 @@ const AboutSiteDialog: React.FC<Props> = ({ destroy }: Props) => {
         </div>
         <div className="border-t mt-3 pt-2 text-sm flex flex-row justify-start items-center">
           <span className="text-gray-500 mr-2">Other projects:</span>
-          <a href="https://github.com/boojack/sticky-notes" className="flex items-center underline text-blue-600 hover:opacity-80">
+          <a
+            href="https://github.com/boojack/sticky-notes"
+            target="_blank"
+            rel="noreferrer"
+            className="flex items-center underline text-blue-600 hover:opacity-80"
+          >
             <img
               className="w-4 h-auto mr-1"
               src="https://raw.githubusercontent.com/boojack/sticky-notes/main/public/sticky-notes.ico"
--- web/src/components/GitHubBadge.tsx
+++ web/src/components/GitHubBadge.tsx
@@ -15,6 +15,8 @@ const GitHubBadge = () => {
     <a
       className="h-7 flex flex-row justify-start items-center border dark:border-zinc-600 rounded cursor-pointer hover:opacity-80"
       href="https://github.com/usememos/memos"
+      target="_blank"
+      rel="noreferrer"
     >
       <div className="apply w-auto h-full px-2 flex flex-row justify-center items-center text-xs bg-gray-100 dark:bg-zinc-700">
         <Icon.Github className="mr-1 w-4 h-4" />
