70356c055b9f5b09d6f8ac13403890c217499758
Steven <stevenlgtm@gmail.com>
2023-03-05 01:00:37 +0800

    chore: add heat map to sidebar

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -64,7 +64,7 @@ const Header = () => {
                 className="px-4 pr-5 py-2 rounded-lg flex flex-row items-center text-lg dark:text-gray-200 hover:bg-white hover:shadow dark:hover:bg-zinc-700"
                 onClick={() => showArchivedMemoDialog()}
               >
-                <Icon.Archive className="mr-4 w-6 h-auto opacity-80" /> {t("common.archive")}
+                <Icon.Archive className="mr-4 w-6 h-auto opacity-80" /> {t("common.archived")}
               </button>
               <button
                 className="px-4 pr-5 py-2 rounded-lg flex flex-row items-center text-lg dark:text-gray-200 hover:bg-white hover:shadow dark:hover:bg-zinc-700"
