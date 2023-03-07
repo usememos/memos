49871bc6c0d04731bcc79767461238a7939c05fe
blessyou <370732889@qq.com>
2023-03-07 09:02:19 +0800

    fix: the browser back button doesnt work for some page

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,7 +2,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-const devProxyServer = "http://localhost:8081/";
+const devProxyServer = "https://memos.flyneko.com/";
 
 // https://vitejs.dev/config/
 export default defineConfig({
