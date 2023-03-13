4e3790ec0d61efcef91a25b66dd86484205c5f6d
CorrectRoadH <a778917369@gmail.com>
2023-03-13 11:51:49 +0800

    feat: beautify file dashboard

--- web/src/pages/ResourcesDashboard.tsx
+++ web/src/pages/ResourcesDashboard.tsx
@@ -15,6 +15,7 @@ import { showCommonDialog } from "../components/Dialog/CommonDialog";
 import showPreviewImageDialog from "../components/PreviewImageDialog";
 import showCreateResourceDialog from "../components/CreateResourceDialog";
 import showChangeResourceFilenameDialog from "../components/ChangeResourceFilenameDialog";
+import MobileHeader from "../components/MobileHeader";
 
 const ResourcesDashboard = () => {
   const { t } = useTranslation();
@@ -135,51 +136,57 @@ const ResourcesDashboard = () => {
   };
 
   return (
-    <>
-      <div className=" flex flex-col justify-start items-start w-full">
-        <p className="title-text">{t("common.resources")}</p>
-      </div>
-      <div className=" flex flex-col justify-start items-start w-full">
-        <div className="w-full flex flex-row justify-between items-center">
-          <div className="flex flex-row justify-start items-center space-x-2">
-            <Button onClick={() => showCreateResourceDialog({})} startDecorator={<Icon.Plus className="w-5 h-auto" />}>
-              {t("common.create")}
-            </Button>
-
-            <Button onClick={() => handleDeleteSelectedBtnClick()} color="danger" startDecorator={<Icon.Trash2 className="w-4 h-auto" />}>
-              {"删除所选资源"}
-            </Button>
-          </div>
-          <div className="flex flex-row justify-end items-center">
-            <Button color="danger" onClick={handleDeleteUnusedResourcesBtnClick} startDecorator={<Icon.Trash2 className="w-4 h-auto" />}>
-              <span>{t("resources.clear")}</span>
-            </Button>
-          </div>
+    <section className="w-full min-h-full flex flex-col justify-start items-center px-4 sm:px-2 sm:pt-4 pb-8 bg-zinc-100 dark:bg-zinc-800">
+      <MobileHeader showSearch={false} />
+      <div className="w-full flex flex-col justify-start items-start px-4 py-3 rounded-xl bg-white dark:bg-zinc-700 text-black dark:text-gray-300">
+        <div className="relative w-full flex flex-row justify-between items-center">
+          <p className="px-2 py-1 flex flex-row justify-start items-center cursor-pointer select-none rounded hover:bg-gray-100 dark:hover:bg-zinc-700">
+            <Icon.Paperclip className="w-5 h-auto mr-1" /> {t("common.resources")}
+          </p>
         </div>
-        {loadingState.isLoading ? (
-          <div className="flex flex-col justify-center items-center w-full h-32">
-            <p className="w-full text-center text-base my-6 mt-8">{t("resources.fetching-data")}</p>
+
+        <div className=" flex flex-col justify-start items-start w-full">
+          <div className="w-full flex flex-row justify-between items-center">
+            <div className="flex flex-row justify-start items-center space-x-2">
+              <Button onClick={() => showCreateResourceDialog({})} startDecorator={<Icon.Plus className="w-5 h-auto" />}>
+                {t("common.create")}
+              </Button>
+
+              <Button onClick={() => handleDeleteSelectedBtnClick()} color="danger" startDecorator={<Icon.Trash2 className="w-4 h-auto" />}>
+                {"删除所选资源"}
+              </Button>
+            </div>
+            <div className="flex flex-row justify-end items-center">
+              <Button color="danger" onClick={handleDeleteUnusedResourcesBtnClick} startDecorator={<Icon.Trash2 className="w-4 h-auto" />}>
+                <span>{t("resources.clear")}</span>
+              </Button>
+            </div>
           </div>
-        ) : (
-          <div className="flex  w-full h-full flex-wrap justify-start">
-            {resources.length === 0 ? (
-              <p className="w-full text-center text-base my-6 mt-8">{t("resources.no-resources")}</p>
-            ) : (
-              resources.map((resource) => (
-                <FileCard
-                  key={resource.id}
-                  resouce={resource}
-                  select={() => handleSelectBtnClick(resource)}
-                  unselect={() => handleUNSelectBtnClick(resource)}
-                  rename={handleRenameBtnClick}
-                  deleteHandle={handleDeleteResourceBtnClick}
-                ></FileCard>
-              ))
-            )}
-          </div>
-        )}
+          {loadingState.isLoading ? (
+            <div className="flex flex-col justify-center items-center w-full h-32">
+              <p className="w-full text-center text-base my-6 mt-8">{t("resources.fetching-data")}</p>
+            </div>
+          ) : (
+            <div className="flex  w-full h-full flex-wrap justify-start">
+              {resources.length === 0 ? (
+                <p className="w-full text-center text-base my-6 mt-8">{t("resources.no-resources")}</p>
+              ) : (
+                resources.map((resource) => (
+                  <FileCard
+                    key={resource.id}
+                    resouce={resource}
+                    select={() => handleSelectBtnClick(resource)}
+                    unselect={() => handleUNSelectBtnClick(resource)}
+                    rename={handleRenameBtnClick}
+                    deleteHandle={handleDeleteResourceBtnClick}
+                  ></FileCard>
+                ))
+              )}
+            </div>
+          )}
+        </div>
       </div>
-    </>
+    </section>
   );
 };
 
