3c257a6291018cc41290f91d4acb9d01cda63738
CorrectRoadH <a778917369@gmail.com>
2023-03-14 23:02:10 +0800

    chore: reserve the searchbar

--- web/src/components/SearchBar.tsx
+++ web/src/components/SearchBar.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState, useRef } from "react";
+import useDebounce from "../hooks/useDebounce";
 import { useFilterStore, useDialogStore, useLayoutStore } from "../store/module";
 import { resolution } from "../utils/layout";
-import Search from "./base/Search";
+import Icon from "./Icon";
 
 const SearchBar = () => {
   const filterStore = useFilterStore();
@@ -44,9 +45,13 @@ const SearchBar = () => {
     }
   }, [layoutStore.state.showHomeSidebar]);
 
-  const queryFunction = () => {
-    filterStore.setTextFilter(queryText.length === 0 ? undefined : queryText);
-  };
+  useDebounce(
+    () => {
+      filterStore.setTextFilter(queryText.length === 0 ? undefined : queryText);
+    },
+    200,
+    [queryText]
+  );
 
   const handleTextQueryInput = (event: React.FormEvent<HTMLInputElement>) => {
     const text = event.currentTarget.value;
@@ -54,13 +59,17 @@ const SearchBar = () => {
   };
 
   return (
-    <Search
-      placeholder="Search memos"
-      inputRef={inputRef}
-      queryText={queryText}
-      queryFunction={queryFunction}
-      handleTextQueryInput={handleTextQueryInput}
-    ></Search>
+    <div className="w-full h-9 flex flex-row justify-start items-center py-2 px-3 rounded-md bg-gray-200 dark:bg-zinc-700">
+      <Icon.Search className="w-4 h-auto opacity-30 dark:text-gray-200" />
+      <input
+        className="flex ml-2 w-24 grow text-sm outline-none bg-transparent dark:text-gray-200"
+        type="text"
+        placeholder="Search memos"
+        ref={inputRef}
+        value={queryText}
+        onChange={handleTextQueryInput}
+      />
+    </div>
   );
 };
 
