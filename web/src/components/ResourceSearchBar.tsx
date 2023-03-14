c829c5dd2afd6adbb62515617450cdc77cefba0d
CorrectRoadH <a778917369@gmail.com>
2023-03-14 23:05:05 +0800

    feat: refator resource Search bar

--- web/src/components/ResourceSearchBar.tsx
+++ web/src/components/ResourceSearchBar.tsx
@@ -1,32 +1,40 @@
 import { useState, useRef } from "react";
-import { useFilterStore, useDialogStore, useLayoutStore } from "../store/module";
-import Search from "./base/Search";
+import Icon from "./Icon";
+import useDebounce from "../hooks/useDebounce";
 
-interface ResourceSearchBarProps{
+interface ResourceSearchBarProps {
   setQuery: (arg0: string) => void;
 }
 const ResourceSearchBar = ({ setQuery }: ResourceSearchBarProps) => {
   const [queryText, setQueryText] = useState("");
   const inputRef = useRef<HTMLInputElement>(null);
 
-  const queryFunction = () => {
-    setQuery(queryText);
-  };
-
   const handleTextQueryInput = (event: React.FormEvent<HTMLInputElement>) => {
     const text = event.currentTarget.value;
     setQueryText(text);
   };
 
+  useDebounce(
+    () => {
+      setQuery(queryText);
+    },
+    200,
+    [queryText]
+  );
+
   return (
     <div className="w-6/12">
-      <Search
-        placeholder="Search Resources"
-        inputRef={inputRef}
-        queryText={queryText}
-        queryFunction={queryFunction}
-        handleTextQueryInput={handleTextQueryInput}
-      ></Search>
+      <div className="w-full h-9 flex flex-row justify-start items-center py-2 px-3 rounded-md bg-gray-200 dark:bg-zinc-700">
+        <Icon.Search className="w-4 h-auto opacity-30 dark:text-gray-200" />
+        <input
+          className="flex ml-2 w-24 grow text-sm outline-none bg-transparent dark:text-gray-200"
+          type="text"
+          placeholder="Search resource "
+          ref={inputRef}
+          value={queryText}
+          onChange={handleTextQueryInput}
+        />
+      </div>
     </div>
   );
 };
