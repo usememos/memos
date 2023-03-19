052ca51256ecfc9f6ef1afa95061e48aba494483
Zeng1998 <acan.coder@gmail.com>
2023-03-19 15:29:19 +0800

    update

--- server/memo.go
+++ server/memo.go
@@ -202,7 +202,6 @@ func (s *Server) registerMemoRoutes(g *echo.Group) {
 		if offset, err := strconv.Atoi(c.QueryParam("offset")); err == nil {
 			memoFind.Offset = &offset
 		}
-		
 		list, err := s.Store.FindMemoList(ctx, memoFind)
 		if err != nil {
 			return echo.NewHTTPError(http.StatusInternalServerError, "Failed to fetch memo list").SetInternal(err)
