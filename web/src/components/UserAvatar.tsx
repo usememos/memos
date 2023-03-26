17ca97ebd1249e65550534f11a5f090110a180fe
Stephen Zhou <hi@hyoban.cc>
2023-03-26 20:43:15 +0800

    fix: avatar is not rounded (#1415)

--- web/src/components/UserAvatar.tsx
+++ web/src/components/UserAvatar.tsx
@@ -6,7 +6,7 @@ interface Props {
 const UserAvatar = (props: Props) => {
   const { avatarUrl, className } = props;
   return (
-    <div className={`${className ?? ""} w-8 h-8 rounded-full bg-gray-100 dark:bg-zinc-800`}>
+    <div className={`${className ?? ""} w-8 h-8 rounded-full overflow-clip bg-gray-100 dark:bg-zinc-800`}>
       <img className="w-full h-auto min-w-full min-h-full object-cover" src={avatarUrl || "/logo.png"} alt="" />
     </div>
   );
