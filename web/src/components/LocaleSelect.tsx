0cddb358c13d634afac5477fa6ed360c3efbaab8
boojack <stevenlgtm@gmail.com>
2023-04-01 21:34:10 +0800

    chore: add Slovenian locale (#1437)
    chore: add sl locale

--- web/src/components/LocaleSelect.tsx
+++ web/src/components/LocaleSelect.tsx
@@ -36,6 +36,7 @@ const LocaleSelect: FC<Props> = (props: Props) => {
       <Option value="hant">繁體中文</Option>
       <Option value="tr">Turkish</Option>
       <Option value="ko">한국어</Option>
+      <Option value="sl">Slovenian</Option>
     </Select>
   );
 };
