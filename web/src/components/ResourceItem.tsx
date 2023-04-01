fab3dac70ace3356a90644fd6805d15854496f90
boojack <stevenlgtm@gmail.com>
2023-04-01 16:38:39 +0800

    chore: remove `useListStyle` hook (#1434)

--- web/src/components/ResourceItem.tsx
+++ web/src/components/ResourceItem.tsx
@@ -22,12 +22,12 @@ const ResourceItem = ({
   };
 
   return (
-    <div key={resource.id} className="px-2 py-2 w-full grid grid-cols-7">
-      <span className="w-full m-auto truncate col-span-1  justify-center">
+    <div key={resource.id} className="px-2 py-2 w-full grid grid-cols-10">
+      <span className="w-4 m-auto truncate justify-center">
         <input type="checkbox" onClick={handleSelectBtnClick}></input>
       </span>
-      <span className="w-full m-auto truncate text-base pr-2 last:pr-0 col-span-1">{resource.id}</span>
-      <span className="w-full m-auto truncate text-base pr-2 last:pr-0 col-span-4" onClick={() => handleRenameBtnClick(resource)}>
+      <span className="w-full m-auto truncate text-base pr-2 last:pr-0 col-span-2">{resource.id}</span>
+      <span className="w-full m-auto truncate text-base pr-2 last:pr-0 col-span-6" onClick={() => handleRenameBtnClick(resource)}>
         {resource.filename}
       </span>
       <div className="w-full flex flex-row justify-between items-center mb-2">
