333b09b2784bd3b79ff2af08dfe746241cf69f18
CorrectRoadH <a778917369@gmail.com>
2023-04-04 20:16:41 +0800

    feat: add snapshot for resourceItemDown

---
+++ web/tests/components/ResourceItemDropdown.test.tsx
@@ -0,0 +1,28 @@
+import ResourceItemDropdown from "../../src/components/ResourceItemDropdown";
+import renderer from "react-test-renderer";
+import { Provider } from "react-redux";
+import store from "../../src/store";
+
+describe("ResourceItemDropdown snapshot", () => {
+  it("render ResourceItemDropdown", () => {
+    const resource: Resource = {
+      id: 1,
+      publicId: "test",
+      filename: "filename",
+      externalLink: "https://www.google.com",
+      size: "test",
+      type: "IMAGE",
+      createdTs: 0,
+      updatedTs: 0,
+      linkedMemoAmount: 0,
+    };
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <ResourceItemDropdown resource={resource} />
+        </Provider>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
