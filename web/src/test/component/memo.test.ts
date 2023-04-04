75cc3ce7ccc3a097450c71f18ce95fe7e0c3c887
CtRdH <a778917369@gmail.com>
2023-04-04 12:41:50 +0800

    feat:change test

--- web/src/test/component/memo.test.ts
+++ web/src/test/component/memo.test.ts
@@ -1,10 +1,30 @@
 import {render, screen} from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Memo from "@/components/Memo";
+import { memo } from 'react';
+
+interface Props {
+  memo: Memo;
+  readonly?: boolean;
+}
+
 
 test('loads and displays greeting', async () => {
   // ARRANGE
-  render(<Memo />)
+  const memoData:Memo = {
+    id: 1,
+    creatorId: 1,
+    createdTs: 1,
+    updatedTs: 1,
+    rowStatus: 'ACTIVE' as RowStatus,
+    content: 'hello there',
+    visibility: 'PUBLIC',
+    pinned: false,
+    creatorName: 'test',
+    resourceList: []
+  
+  }
+  render(<Memo memo={memoData} ></Memo>)
 
   // ASSERT
   expect(screen.getByRole('heading')).toHaveTextContent('hello there')
