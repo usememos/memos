20ab630198671c3752329c94efbe3a217fea77d0
CorrectRoadH <a778917369@gmail.com>
2023-04-05 20:48:18 +0800

    feat: add test for function

---
+++ web/tests/utils/convertLanguageCodeToLocals.test.ts
@@ -0,0 +1,15 @@
+import { convertLanguageCodeToLocale } from "@/utils/convertLanguageCodeToLocale";
+import { describe } from "vitest";
+
+describe("convertLanguageCodeToLocale", () => {
+  it("test zh hant convert to zh-Hant", () => {
+    expect(convertLanguageCodeToLocale("zh-TW")).toBe("hant");
+    expect(convertLanguageCodeToLocale("zh-HK")).toBe("hant");
+  });
+  it("test zh  convert to zh", () => {
+    expect(convertLanguageCodeToLocale("zh-CN")).toBe("zh");
+  });
+  it("test convert language from first work", () => {
+    expect(convertLanguageCodeToLocale("en-US")).toBe("en");
+  });
+});
