24592b8ba955abf0f5a3875016199518c05f12d3
CorrectRoadH <a778917369@gmail.com>
2023-04-06 14:04:39 +0800

    change the name

--- web/tests/hooks/useToggle.test.tsx
+++ web/tests/hooks/useToggle.test.tsx
@@ -3,14 +3,14 @@ import useToggle from "../../src/hooks/useToggle";
 import { describe, expect, it } from "vitest";
 
 describe("useToggle", () => {
-  it("toggle should toggle the value to true", () => {
+  it("useToggle should toggle the value to true", () => {
     const { result } = renderHook(() => useToggle());
     act(() => {
       result.current[1]();
     });
     expect(result.current[0]).toBe(true);
   });
-  it("toggle should toggle the value to false", () => {
+  it("useToggle should toggle the value to false", () => {
     const { result } = renderHook(() => useToggle(true));
     act(() => {
       result.current[1]();
