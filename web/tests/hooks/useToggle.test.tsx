461aa9b0dc34e6c4703a2c9a2304da692e53e622
CorrectRoadH <a778917369@gmail.com>
2023-04-06 14:29:27 +0800

    change useToggle value

--- web/tests/hooks/useToggle.test.tsx
+++ web/tests/hooks/useToggle.test.tsx
@@ -5,15 +5,19 @@ import { describe, expect, it } from "vitest";
 describe("useToggle", () => {
   it("useToggle should toggle the value to true", () => {
     const { result } = renderHook(() => useToggle());
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [_, toggle] = result.current;
     act(() => {
-      result.current[1]();
+      toggle();
     });
     expect(result.current[0]).toBe(true);
   });
   it("useToggle should toggle the value to false", () => {
     const { result } = renderHook(() => useToggle(true));
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [_, toggle] = result.current;
     act(() => {
-      result.current[1]();
+      toggle();
     });
     expect(result.current[0]).toBe(false);
   });
