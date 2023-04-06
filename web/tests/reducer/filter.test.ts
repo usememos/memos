5b6b77e50d311f388057ed6f78c354bdabf5a523
CorrectRoadH <a778917369@gmail.com>
2023-04-06 15:07:18 +0800

    delete unused line

--- web/tests/reducer/filter.test.ts
+++ web/tests/reducer/filter.test.ts
@@ -17,20 +17,17 @@ interface State {
 describe("setFilter", () => {
   it("setFilter should add filter to state", () => {
     const setFilterOutputState1 = reducer({} as State, setFilter({ tag: "study" }));
-
     expect(setFilterOutputState1).toEqual({
       tag: "study",
     });
 
     const setFilterOutputState2 = reducer(setFilterOutputState1 as State, setFilter({ text: "content" }));
-
     expect(setFilterOutputState2).toEqual({
       tag: "study",
       text: "content",
     });
 
     const setFilterOutputState3 = reducer(setFilterOutputState2 as State, setFilter({ visibility: "PUBLIC" }));
-
     expect(setFilterOutputState3).toEqual({
       tag: "study",
       text: "content",
@@ -38,7 +35,6 @@ describe("setFilter", () => {
     });
 
     const setFilterOutputState4 = reducer(setFilterOutputState3 as State, setFilter({ tag: "learn" }));
-
     expect(setFilterOutputState4).toEqual({
       tag: "learn",
       text: "content",
