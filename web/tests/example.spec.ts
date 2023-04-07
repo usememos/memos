98b687d2e6c0c63845b2e1431937831018af3f25
CorrectRoadH <a778917369@gmail.com>
2023-04-07 13:28:56 +0800

    add multiple test for e2e

--- web/tests/example.spec.ts
+++ web/tests/example.spec.ts
@@ -1,7 +1,14 @@
 import { test, expect, Page } from "@playwright/test";
+import locale from "../src/locales/en.json";
 
 let page: Page;
 test.describe.configure({ mode: "serial" });
+
+// console.log(data);
+test.use({
+  locale: "en-US",
+  timezoneId: "Europe/Berlin",
+});
 
 test.beforeAll(async ({ browser }) => {
   page = await browser.newPage();
@@ -20,22 +27,22 @@ test("login", async () => {
   await page.goto("http://localhost:3001/");
 
   // Click the get started link.
-  await page.getByRole("link", { name: "Sign in" }).click();
+  await page.getByRole("link", { name: locale.common["sign-in"] }).click();
 
   // Expects the URL to contain intro.
   await expect(page).toHaveURL(/.*auth/);
-  await page.getByRole("button", { name: "Sign in" }).click();
+  await page.getByRole("button", { name: locale.common["sign-in"] }).click();
 
-  await expect(page.getByText("Home")).toBeVisible();
+  await expect(page.getByText(locale.common.home)).toBeVisible();
 });
 
 test("Write Memos", async () => {
   await page.goto("http://localhost:3001/");
   const RandomString = Math.random().toString(36).substring(7);
-  await expect(page.getByRole("button", { name: "Save" })).toBeDisabled();
+  await expect(page.getByRole("button", { name: locale.editor.save })).toBeDisabled();
   await page.getByPlaceholder("Any thoughts...").fill(RandomString);
-  await expect(page.getByRole("button", { name: "Save" })).toBeEnabled();
-  await page.getByRole("button", { name: "Save" }).click();
+  await expect(page.getByRole("button", { name: locale.editor.save })).toBeEnabled();
+  await page.getByRole("button", { name: locale.editor.save }).click();
   await expect(page.getByText(RandomString)).toBeVisible();
 });
 
