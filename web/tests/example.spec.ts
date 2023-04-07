3fa709e2faab6647c8c6ae919f93b40c201b56f7
CorrectRoadH <a778917369@gmail.com>
2023-04-07 10:25:56 +0800

    add base e2e test

--- web/tests/example.spec.ts
+++ web/tests/example.spec.ts
@@ -1,18 +1,44 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, Page } from "@playwright/test";
 
-test("has title", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+let page: Page;
+test.describe.configure({ mode: "serial" });
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test.beforeAll(async ({ browser }) => {
+  page = await browser.newPage();
 });
 
-test("get started link", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+test.afterAll(async () => {
+  await page.close();
+});
+
+test("has title", async ({ page }) => {
+  await page.goto("http://localhost:3001/");
+  await expect(page).toHaveTitle(/memos/);
+});
+
+test("login", async () => {
+  await page.goto("http://localhost:3001/");
 
   // Click the get started link.
-  await page.getByRole("link", { name: "Get started" }).click();
+  await page.getByRole("link", { name: "Sign in" }).click();
 
   // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+  await expect(page).toHaveURL(/.*auth/);
+  await page.getByRole("button", { name: "Sign in" }).click();
+
+  await expect(page.getByText("Home")).toBeVisible();
+});
+
+test("Write Memos", async () => {
+  await page.goto("http://localhost:3001/");
+  const RandomString = Math.random().toString(36).substring(7);
+  await expect(page.getByRole("button", { name: "Save" })).toBeDisabled();
+  await page.getByPlaceholder("Any thoughts...").fill(RandomString);
+  await expect(page.getByRole("button", { name: "Save" })).toBeEnabled();
+  await page.getByRole("button", { name: "Save" }).click();
+  await expect(page.getByText(RandomString)).toBeVisible();
+});
+
+test("Filter Memos", async () => {
+  await page.goto("http://localhost:3001/");
 });
