fc36cc3100b7891d3c907a7d3a72c86c097fd12e
Steven <stevenlgtm@gmail.com>
2023-04-08 18:10:24 +0800

    chore: add ignore version upgrade setting

--- web/src/components/Settings/SystemSection.tsx
+++ web/src/components/Settings/SystemSection.tsx
@@ -10,6 +10,7 @@ import "@/less/settings/system-section.less";
 interface State {
   dbSize: number;
   allowSignUp: boolean;
+  ignoreUpgrade: boolean;
   disablePublicMemos: boolean;
   additionalStyle: string;
   additionalScript: string;
@@ -31,6 +32,7 @@ const SystemSection = () => {
   const [state, setState] = useState<State>({
     dbSize: systemStatus.dbSize,
     allowSignUp: systemStatus.allowSignUp,
+    ignoreUpgrade: systemStatus.ignoreUpgrade,
     additionalStyle: systemStatus.additionalStyle,
     additionalScript: systemStatus.additionalScript,
     disablePublicMemos: systemStatus.disablePublicMemos,
@@ -71,6 +73,17 @@ const SystemSection = () => {
     });
     await api.upsertSystemSetting({
       name: "allow-signup",
+      value: JSON.stringify(value),
+    });
+  };
+
+  const handleIgnoreUpgradeChanged = async (value: boolean) => {
+    setState({
+      ...state,
+      ignoreUpgrade: value,
+    });
+    await api.upsertSystemSetting({
+      name: "ignore-upgrade",
       value: JSON.stringify(value),
     });
   };
@@ -188,6 +201,10 @@ const SystemSection = () => {
       <div className="form-label">
         <span className="normal-text">{t("setting.system-section.allow-user-signup")}</span>
         <Switch checked={state.allowSignUp} onChange={(event) => handleAllowSignUpChanged(event.target.checked)} />
+      </div>
+      <div className="form-label">
+        <span className="normal-text">Ignore version upgrade</span>
+        <Switch checked={state.ignoreUpgrade} onChange={(event) => handleIgnoreUpgradeChanged(event.target.checked)} />
       </div>
       <div className="form-label">
         <span className="normal-text">{t("setting.system-section.disable-public-memos")}</span>
--- web/src/layouts/Root.tsx
+++ web/src/layouts/Root.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "@/components/Header";
-import UpdateVersionBanner from "@/components/UpdateVersionBanner";
+import UpgradeVersionBanner from "@/components/UpgradeVersionBanner";
 
 function Root() {
   return (
     <div className="w-full min-h-full bg-zinc-100 dark:bg-zinc-800">
       <div className="w-full h-auto flex flex-col justify-start items-center">
-        <UpdateVersionBanner />
+        <UpgradeVersionBanner />
       </div>
       <div className="w-full max-w-6xl mx-auto flex flex-row justify-center items-start">
         <Header />
