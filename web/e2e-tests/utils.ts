400f1d5ee01c328848b74a3ac2e448b62cfb9259
CorrectRoadH <a778917369@gmail.com>
2023-04-10 23:41:41 +0800

    feat: extrace screenshot

--- web/e2e-tests/utils.ts
+++ web/e2e-tests/utils.ts
@@ -2,6 +2,10 @@ import { expect, Page } from "@playwright/test";
 import locale from "../src/locales/en.json";
 import { baseHost } from "./fixtures";
 
+async function screenshot(page: Page, name: string) {
+  await page.screenshot({ path: `../playwright-report/screenshot/${name}.png`, fullPage: true });
+}
+
 async function writeMemo(page: Page, content: string) {
   await expect(page.getByRole("button", { name: locale.editor.save })).toBeDisabled();
   await page.getByPlaceholder("Any thoughts...").fill(content);
@@ -11,10 +15,10 @@ async function writeMemo(page: Page, content: string) {
 
 async function login(page: Page, username: string, password: string) {
   page.goto(`${baseHost}/`);
-  await page.screenshot({ path: "../playwright-report/screenshot/main.png", fullPage: true });
+  await screenshot(page, "explore");
   await page.getByRole("link", { name: locale.common["sign-in"] }).click();
-  await page.screenshot({ path: "../playwright-report/screenshot/login.png", fullPage: true });
   await expect(page).toHaveURL(/.*auth/);
+  await screenshot(page, "sign-in");
   await page.locator('input[type="text"]').fill(username);
   await page.locator('input[type="password"]').fill(password);
   await page.getByRole("button", { name: locale.common["sign-in"] }).click();
@@ -23,6 +27,7 @@ async function login(page: Page, username: string, password: string) {
 
 async function SignUp(page: Page, username: string, password: string) {
   await page.goto(`${baseHost}/`);
+  await screenshot(page, "sign-up");
   await page.locator('input[type="text"]').fill(username);
   await page.locator('input[type="password"]').fill(password);
   await page.getByRole("button", { name: locale.auth["signup-as-host"] }).click();
@@ -31,6 +36,7 @@ async function SignUp(page: Page, username: string, password: string) {
 async function Review(page: Page) {
   await page.goto(`${baseHost}/`);
   await page.getByRole("link", { name: locale["daily-review"]["title"] }).click();
+  await screenshot(page, "review");
 }
 
 export { writeMemo, login, SignUp, Review };
