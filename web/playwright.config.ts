b77146cf04a107ff0bbb3bc6ddacbc2d2def5e2f
CorrectRoadH <a778917369@gmail.com>
2023-04-10 23:07:40 +0800

    change name of test

--- web/playwright.config.ts
+++ web/playwright.config.ts
@@ -10,7 +10,7 @@ import { defineConfig, devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: "./tests",
+  testDir: "./e2e-tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
