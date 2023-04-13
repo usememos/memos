0c70efeed9ad1c393b1f677e5204376b895f853f
CorrectRoadH <a778917369@gmail.com>
2023-04-13 09:54:02 +0800

    add more test cases

--- test/store/resource_test.go
+++ test/store/resource_test.go
@@ -23,6 +23,7 @@ func TestResourceStore(t *testing.T) {
 		PublicID:     "a02748e2-9b56-46b2-8b1f-72d686d52f77",
 	})
 	require.NoError(t, err)
+
 	correctFilename := "test.epub"
 	incorrectFilename := "test.png"
 	res, err := store.FindResource(ctx, &api.ResourceFind{
@@ -34,8 +35,24 @@ func TestResourceStore(t *testing.T) {
 		Filename: &incorrectFilename,
 	})
 	require.Error(t, err)
+
+	correctCreatorID := 101
+	incorrectCreatorID := 102
+	res, err = store.FindResource(ctx, &api.ResourceFind{
+		CreatorID: &correctCreatorID,
+	})
+	require.NoError(t, err)
+	res, err = store.FindResource(ctx, &api.ResourceFind{
+		CreatorID: &incorrectCreatorID,
+	})
+	require.Error(t, err)
+
 	err = store.DeleteResource(ctx, &api.ResourceDelete{
 		ID: 1,
 	})
 	require.NoError(t, err)
+	err = store.DeleteResource(ctx, &api.ResourceDelete{
+		ID: 2,
+	})
+	require.Error(t, err)
 }
