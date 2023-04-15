29c95ff7b2f5901cdb854243d9ad5831645adb0f
Steven <stevenlgtm@gmail.com>
2023-04-15 09:07:43 +0800

    chore: regenerate yarn lock file

--- web/src/i18n.ts
+++ web/src/i18n.ts
@@ -3,7 +3,6 @@ import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import toast from "react-hot-toast";
 
-// eslint-disable-next-line prettier/prettier
 export const availableLocales = [
   "de",
   "en",
