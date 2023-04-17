a85dda0628362ff36eca057ccf74556bcfc39c3a
Steven <stevenlgtm@gmail.com>
2023-04-17 21:30:03 +0800

    feat: server tests

--- test/test.go
+++ test/test.go
@@ -2,19 +2,34 @@ package test
 
 import (
 	"fmt"
+	"net"
 	"testing"
 
 	"github.com/usememos/memos/server/profile"
 	"github.com/usememos/memos/server/version"
 )
 
+func getUnusedPort() int {
+	// Get a random unused port
+	listener, err := net.Listen("tcp", "localhost:0")
+	if err != nil {
+		panic(err)
+	}
+	defer listener.Close()
+
+	// Get the port number
+	port := listener.Addr().(*net.TCPAddr).Port
+	return port
+}
+
 func GetTestingProfile(t *testing.T) *profile.Profile {
 	// Get a temporary directory for the test data.
 	dir := t.TempDir()
 	mode := "prod"
+	port := getUnusedPort()
 	return &profile.Profile{
 		Mode:    mode,
-		Port:    8082,
+		Port:    port,
 		Data:    dir,
 		DSN:     fmt.Sprintf("%s/memos_%s.db", dir, mode),
 		Version: version.GetCurrentVersion(mode),
