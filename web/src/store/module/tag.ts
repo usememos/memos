202743890c93e7edef01af03c87a947ba967a2b7
Eren Ertas <eren.ertas@d-teknoloji.com.tr>
2023-04-17 08:35:42 +0300

    #1522 Tags shouldn't be case sensitive

--- web/src/store/module/tag.ts
+++ web/src/store/module/tag.ts
@@ -2,26 +2,45 @@ import * as api from "@/helpers/api";
 import store, { useAppSelector } from "..";
 import { deleteTag, setTags, upsertTag } from "../reducer/tag";
 import { useUserStore } from "./";
+import { isString } from "lodash-es";
 
 export const useTagStore = () => {
   const state = useAppSelector((state) => state.tag);
   const userStore = useUserStore();
+
+  const checkTagNameIsDuplicated = async (tagName: string): Promise<boolean | string> => {
+    tagName = tagName.toLowerCase().replace(/[.,/#!$%^&*;:{}?=\-_`~()]/g, "");
+    const data = await getTags();
+    if (!data.includes(tagName)) {
+      return tagName;
+    }
+    return false;
+  };
+
+  const getTags = async (): Promise<string[]> => {
+    const tagFind: TagFind = {};
+    if (userStore.isVisitorMode()) {
+      tagFind.creatorId = userStore.getUserIdFromPath();
+    }
+    const { data } = (await api.getTagList(tagFind)).data;
+    return data;
+  };
+
   return {
     state,
     getState: () => {
       return store.getState().tag;
     },
     fetchTags: async () => {
-      const tagFind: TagFind = {};
-      if (userStore.isVisitorMode()) {
-        tagFind.creatorId = userStore.getUserIdFromPath();
-      }
-      const { data } = (await api.getTagList(tagFind)).data;
+      const data = await getTags();
       store.dispatch(setTags(data));
     },
     upsertTag: async (tagName: string) => {
-      await api.upsertTag(tagName);
-      store.dispatch(upsertTag(tagName));
+      const retValue = await checkTagNameIsDuplicated(tagName);
+      if (!!retValue && isString(retValue)) {
+        await api.upsertTag(retValue);
+        store.dispatch(upsertTag(retValue));
+      }
     },
     deleteTag: async (tagName: string) => {
       await api.deleteTag(tagName);
