a04a0dc7936c93daa33381c84267d9c874f47b8f
CorrectRoadH <a778917369@gmail.com>
2023-04-18 21:22:05 +0800

    remove memos snap test because it contain time info

--- web/tests/components/Memos.test.tsx
+++ web/tests/components/Memos.test.tsx
@@ -28,28 +28,4 @@ describe("Memo", () => {
     );
     screen.getByText(/Hello!/i);
   });
-  it("memo snapshot", () => {
-    const memo: Memo = {
-      id: 123,
-      creatorId: 1234,
-      createdTs: 0,
-      updatedTs: 0,
-      rowStatus: "NORMAL" as RowStatus,
-      content: "Hello!",
-      visibility: "PUBLIC" as Visibility,
-      pinned: false,
-      creatorName: "test",
-      resourceList: [],
-    };
-    const tree = renderer
-      .create(
-        <Provider store={store}>
-          <BrowserRouter>
-            <Memo memo={memo} />
-          </BrowserRouter>
-        </Provider>
-      )
-      .toJSON();
-    expect(tree).toMatchSnapshot();
-  });
 });
