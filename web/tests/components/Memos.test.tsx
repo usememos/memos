396f5f32e2a0b5fa7984d1cdf114cc7c9219196e
CorrectRoadH <a778917369@gmail.com>
2023-04-19 10:40:15 +0800

    add user data for memo component

--- web/tests/components/Memos.test.tsx
+++ web/tests/components/Memos.test.tsx
@@ -3,9 +3,34 @@ import Memo from "@/components/Memo";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "@/store";
+import { setUser } from "@/store/reducer/user";
 
 describe("Memo", () => {
   it("Memo should render the info of memo", () => {
+    const user: User = {
+      id: 1,
+      createdTs: 0,
+      updatedTs: 0,
+      rowStatus: "NORMAL",
+      username: "hello",
+      role: "HOST",
+      email: "test@gmail.com",
+      nickname: "test user",
+      openId: "12341234",
+      avatarUrl: "",
+      userSettingList: [],
+      setting: {
+        locale: "en",
+        appearance: "system",
+        memoVisibility: "PUBLIC" as Visibility,
+      },
+      localSetting: {
+        enableDoubleClickEditing: true,
+        dailyReviewTimeOffset: 1,
+        enableAutoCollapse: true,
+      },
+    };
+    store.dispatch(setUser(user));
     const memo: Memo = {
       id: 123,
       creatorId: 1234,
