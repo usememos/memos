097eef13f92e249ddb7059110d367ea60a969892
CorrectRoadH <a778917369@gmail.com>
2023-04-28 12:06:50 +0800

    delete unnecessary export

--- web/src/hooks/useEvent.ts
+++ web/src/hooks/useEvent.ts
@@ -4,7 +4,7 @@ const useIsoMorphicEffect = (effect: EffectCallback, deps?: DependencyList | und
   useEffect(effect, deps);
 };
 
-export default function useLatestValue<T>(value: T) {
+function useLatestValue<T>(value: T) {
   const cache = useRef(value);
 
   useIsoMorphicEffect(() => {
@@ -14,9 +14,12 @@ export default function useLatestValue<T>(value: T) {
   return cache;
 }
 
-export const useEvent =
-  // TODO: Add React.useEvent ?? once the useEvent hook is available
-  function useEvent<F extends (...args: any[]) => any, P extends any[] = Parameters<F>, R = ReturnType<F>>(cb: (...args: P) => R) {
-    const cache = useLatestValue(cb);
-    return React.useCallback((...args: P) => cache.current(...args), [cache]);
-  };
+// TODO: Add React.useEvent ?? once the useEvent hook is available
+const useEvent = function useEvent<F extends (...args: any[]) => any, P extends any[] = Parameters<F>, R = ReturnType<F>>(
+  cb: (...args: P) => R
+) {
+  const cache = useLatestValue(cb);
+  return React.useCallback((...args: P) => cache.current(...args), [cache]);
+};
+
+export default useEvent;
