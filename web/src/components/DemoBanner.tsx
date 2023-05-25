58b4b1c537286978cd01cf76c29fa7f59d8767f7
Steven <stevenlgtm@gmail.com>
2023-05-26 00:45:08 +0800

    chore: update demo banner style

--- web/src/components/DemoBanner.tsx
+++ web/src/components/DemoBanner.tsx
@@ -23,9 +23,9 @@ const DemoBanner: React.FC = () => {
   if (!state.show) return null;
 
   return (
-    <div className="flex flex-row items-center justify-center w-full py-2 text-lg font-medium dark:text-gray-300 bg-white dark:bg-zinc-700 shadow">
+    <div className="flex flex-row items-center justify-center w-full py-2 text-sm sm:text-lg font-medium dark:text-gray-300 bg-white dark:bg-zinc-700 shadow">
       <div className="w-full max-w-6xl px-4 flex flex-row justify-between items-center gap-x-3">
-        <span>A lightweight, self-hosted memo hub. Open Source and Free forever.</span>
+        <span>✨ A lightweight, self-hosted memo hub. Open Source and Free forever. ✨</span>
         <a className="btn-primary shadow" href="https://usememos.com/docs/install/docker" target="_blank">
           Install
           <Icon.ExternalLink className="w-4 h-auto ml-1" />
