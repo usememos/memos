43819b021eb1368a3228b5b83d661487a4dded87
boojack <stevenlgtm@gmail.com>
2023-05-25 19:29:30 +0800

    chore: add demo banner (#1739)

--- web/src/layouts/Root.tsx
+++ web/src/layouts/Root.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "@/components/Header";
 import UpgradeVersionBanner from "@/components/UpgradeVersionBanner";
+import DemoBanner from "@/components/DemoBanner";
 
 function Root() {
   return (
     <div className="w-full min-h-full bg-zinc-100 dark:bg-zinc-800">
       <div className="w-full h-auto flex flex-col justify-start items-center">
+        <DemoBanner />
         <UpgradeVersionBanner />
       </div>
       <div className="w-full max-w-6xl mx-auto flex flex-row justify-center items-start">
