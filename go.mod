360141a1fd6593a1b27e7904b2d15fdf6ebde203
CorrectRoadH <a778917369@gmail.com>
2023-07-12 22:37:05 +0800

    feat: implment backend function

--- go.mod
+++ go.mod
@@ -27,6 +27,8 @@ require (
 )
 
 require (
+	github.com/CorrectRoadH/echo-sse v0.1.4 // indirect
+	github.com/PullRequestInc/go-gpt3 v1.1.15 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
--- server/server.go
+++ server/server.go
@@ -64,6 +64,15 @@ func NewServer(ctx context.Context, profile *profile.Profile, store *store.Store
 	}))
 
 	e.Use(middleware.TimeoutWithConfig(middleware.TimeoutConfig{
+		Skipper: func(c echo.Context) bool {
+			// this is a hack to skip timeout for openai chat streaming
+			// because streaming require to flush response. But the timeout middleware will break it.
+			if c.Request().URL.Path == "/api/openai/chat-streaming" {
+				return true
+			} else {
+				return false
+			}
+		},
 		ErrorMessage: "Request timeout",
 		Timeout:      30 * time.Second,
 	}))
