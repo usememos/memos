a8bdccee901dcc754f00c76c0be93ba77f8e4fda
CorrectRoadH <a778917369@gmail.com>
2023-07-16 10:43:11 +0800

    eslint

--- web/src/components/MemoChat/ChatMessage.tsx
+++ web/src/components/MemoChat/ChatMessage.tsx
@@ -1,11 +1,11 @@
 import { Message } from "@/store/zustand/message";
 import { marked } from "@/labs/marked";
 import Icon from "@/components/Icon";
-
 import Dropdown from "../kit/Dropdown";
 import { useMemoStore } from "@/store/module";
 import toast from "react-hot-toast";
 import { useTranslate } from "@/utils/i18n";
+
 interface MessageProps {
   index: number;
   message: Message;
