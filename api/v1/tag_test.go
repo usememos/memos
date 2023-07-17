8c6153167181b483d77df726071f844242731711
Felipe Martínez <pipernene@gmail.com>
2023-07-17 19:53:07 +0200

    fix: exclude all punctuation chars except underscore in tags (#1974)
    * Change tag regex
    
    * Update tests
    
    * Add more tag tests

--- api/v1/tag_test.go
+++ api/v1/tag_test.go
@@ -2,6 +2,8 @@ package v1
 
 import (
 	"testing"
+
+	"golang.org/x/exp/slices"
 )
 
 func TestFindTagListFromMemoContent(t *testing.T) {
@@ -35,12 +37,16 @@ func TestFindTagListFromMemoContent(t *testing.T) {
 		},
 		{
 			memoContent: "#tag1 http://123123.com?123123#tag2 \n#tag3  #tag4 http://123123.com?123123#tag2) ",
-			want:        []string{"tag1", "tag2", "tag2)", "tag3", "tag4"},
+			want:        []string{"tag1", "tag2", "tag3", "tag4"},
+		},
+		{
+			memoContent: "#tag1,#tag2! #tag3.. #tag_4",
+			want:        []string{"tag1", "tag2", "tag3", "tag_4"},
 		},
 	}
 	for _, test := range tests {
 		result := findTagListFromMemoContent(test.memoContent)
-		if len(result) != len(test.want) {
+		if !slices.Equal(result, test.want) {
 			t.Errorf("Find tag list %s: got result %v, want %v.", test.memoContent, result, test.want)
 		}
 	}
