80da91f24a04c3b69efa3290fc8f0a7ff9ccbaae
maciej-ka <maciej.kasprzyk.it@gmail.com>
2023-07-30 11:17:24 +0200

    feat: filter suggestions by partially typed tag name

--- web/src/components/MemoEditor/Editor/TagSuggestions.tsx
+++ web/src/components/MemoEditor/Editor/TagSuggestions.tsx
@@ -9,22 +9,20 @@ type Position = { left: number; top: number; height: number };
 
 const TagSuggestions = ({ textareaRef }: Props) => {
   const [position, setPosition] = useState<Position | null>(null);
-
   const hide = () => setPosition(null);
 
   const getCurrentWord = (): [word: string, startIndex: number] => {
     if (!textareaRef.current) return ["", 0];
     const cursorPos = textareaRef.current.selectionEnd;
-    const before = textareaRef.current.value.slice(0, cursorPos).match(/\S*$/) || [""];
-    const ahead = textareaRef.current.value.slice(cursorPos).match(/^\S*/) || [""];
+    const before = textareaRef.current.value.slice(0, cursorPos).match(/\S*$/) || {0: "", index: cursorPos};
+    const ahead = textareaRef.current.value.slice(cursorPos).match(/^\S*/) || {0: ""};
     return [before[0] + ahead[0], before.index || cursorPos];
   };
 
   const handleKeyDown = (e: KeyboardEvent) => {
     const isArrowKey = ["ArrowLeft", "ArrowRight", "ArrowDown", "ArrowUp"].includes(e.code);
-    if (isArrowKey) hide();
+    if (isArrowKey || ['Tab', 'Escape'].includes(e.code)) hide();
   };
-
   const handleInput = () => {
     if (!textareaRef.current) return;
     const [word, index] = getCurrentWord();
@@ -44,14 +42,19 @@ const TagSuggestions = ({ textareaRef }: Props) => {
   useEffect(registerListeners, [!!textareaRef.current]);
 
   const { tags } = useTagStore().state;
+  const getSuggestions = () => {
+    const phrase = getCurrentWord()[0].slice(1);
+    return tags.filter((tag) => tag.startsWith(phrase)).slice(0, 5);
+  }
+  const suggestions = getSuggestions();
 
-  if (!position) return null;
+  if (!position || !suggestions.length) return null;
   return (
     <div
       className="z-2 absolute rounded font-mono bg-zinc-200 dark:bg-zinc-600"
       style={{ left: position.left - 6, top: position.top + position.height + 2 }}
     >
-      {tags.map((tag) => (
+      {suggestions.map((tag) => (
         <div
           key={tag}
           className="rounded p-1 px-2 z-1000 text-sm dark:text-gray-300 cursor-pointer hover:bg-zinc-300 dark:hover:bg-zinc-700"
