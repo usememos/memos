95588542f9940c3ab431e576a177f26ac61200d2
boojack <stevenlgtm@gmail.com>
2023-08-13 23:34:17 +0800

    chore: upgrade version to `0.14.4` (#2132)

--- server/version/version.go
+++ server/version/version.go
@@ -9,10 +9,10 @@ import (
 
 // Version is the service current released version.
 // Semantic versioning: https://semver.org/
-var Version = "0.14.3"
+var Version = "0.14.4"
 
 // DevVersion is the service current development version.
-var DevVersion = "0.14.3"
+var DevVersion = "0.14.4"
 
 func GetCurrentVersion(mode string) string {
 	if mode == "dev" || mode == "demo" {
