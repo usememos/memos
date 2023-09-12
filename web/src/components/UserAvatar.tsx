439d88f06b1b26cc04fffe12b6fd8b7909d5a3af
Steven <stevenlgtm@gmail.com>
2023-09-12 23:35:10 +0800

    chore: fix user avatar style

--- web/src/components/UserAvatar.tsx
+++ web/src/components/UserAvatar.tsx
@@ -8,7 +8,7 @@ interface Props {
 const UserAvatar = (props: Props) => {
   const { avatarUrl, className } = props;
   return (
-    <div className={classNames(`w-8 h-auto overflow-clip rounded-full`, className)}>
+    <div className={classNames(`w-8 h-8 overflow-clip rounded-full`, className)}>
       <img
         className="w-full h-auto rounded-full min-w-full min-h-full object-cover dark:opacity-80"
         src={avatarUrl || "/logo.webp"}
