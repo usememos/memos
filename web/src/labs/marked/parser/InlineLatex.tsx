1675b51c88da25c5def43867a21367dfcefe2149
Kada Liao <kadaliao@gmail.com>
2023-09-13 15:47:06 +0800

    fix: multiple inline latex parsing

--- web/src/labs/marked/parser/InlineLatex.tsx
+++ web/src/labs/marked/parser/InlineLatex.tsx
@@ -1,24 +1,20 @@
 import TeX from "@matejmazur/react-katex";
 import "katex/dist/katex.min.css";
 
-export const LATEX_INLINE_REG = /\$(.+?)\$|\\\(([^\\]+)\\\)/g;
+export const LATEX_INLINE_REG = /\$(.+?)\$|\\\((.+?)\\\)/;
 
 const inlineRenderer = (rawStr: string) => {
   const matchResult = LATEX_INLINE_REG.exec(rawStr);
-  if (!matchResult) {
-    return rawStr;
+  if (matchResult) {
+    let latexCode = "";
+    if (matchResult[1]) {
+      latexCode = matchResult[1];
+    } else if (matchResult[2]) {
+      latexCode = matchResult[2];
+    }
+    return <TeX key={latexCode}>{latexCode}</TeX>;
   }
-
-  let latexCode = "";
-
-  if (matchResult[1]) {
-    // $
-    latexCode = matchResult[1];
-  } else if (matchResult[2]) {
-    // \( and \)
-    latexCode = matchResult[2];
-  }
-  return <TeX>{latexCode}</TeX>;
+  return rawStr;
 };
 
 export default {
