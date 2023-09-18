1c27824e58b2891d3cb5e618d34aba8022ecae99
Steven <stevenlgtm@gmail.com>
2023-09-18 22:54:44 +0800

    chore: upgrade version

--- server/version/version.go
+++ server/version/version.go
@@ -9,10 +9,10 @@ import (
 
 // Version is the service current released version.
 // Semantic versioning: https://semver.org/
-var Version = "0.15.0"
+var Version = "0.15.1"
 
 // DevVersion is the service current development version.
-var DevVersion = "0.15.0"
+var DevVersion = "0.15.1"
 
 func GetCurrentVersion(mode string) string {
 	if mode == "dev" || mode == "demo" {
