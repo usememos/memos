d3a6fa50d6de9d42b432d4f830ed05c0f30dc6f8
Athurg Gooth <athurg@gooth.org>
2023-09-26 19:04:07 +0800

    chore: move sql code of Resource into driver (#2286)
    Move sql code of Resource into driver

--- store/driver.go
+++ store/driver.go
@@ -9,6 +9,11 @@ import (
 type Driver interface {
 	CreateActivity(ctx context.Context, create *Activity) (*Activity, error)
 
+	CreateResource(ctx context.Context, create *Resource) (*Resource, error)
+	ListResources(ctx context.Context, find *FindResource) ([]*Resource, error)
+	UpdateResource(ctx context.Context, update *UpdateResource) (*Resource, error)
+	DeleteResource(ctx context.Context, delete *DeleteResource) error
+
 	UpsertSystemSetting(ctx context.Context, upsert *SystemSetting) (*SystemSetting, error)
 	ListSystemSettings(ctx context.Context, find *FindSystemSetting) ([]*SystemSetting, error)
 
