794a39a73131e15d52cfb8f8f8c29fb4e4ceb896
Athurg Gooth <athurg@gooth.org>
2023-09-26 18:26:49 +0800

    Move SQL code of UserSetting into Driver

--- store/user_setting.go
+++ store/user_setting.go
@@ -3,10 +3,6 @@ package store
 import (
 	"context"
 	"database/sql"
-	"errors"
-	"strings"
-
-	"google.golang.org/protobuf/encoding/protojson"
 
 	storepb "github.com/usememos/memos/proto/gen/store"
 )
@@ -23,60 +19,18 @@ type FindUserSetting struct {
 }
 
 func (s *Store) UpsertUserSetting(ctx context.Context, upsert *UserSetting) (*UserSetting, error) {
-	stmt := `
-		INSERT INTO user_setting (
-			user_id, key, value
-		)
-		VALUES (?, ?, ?)
-		ON CONFLICT(user_id, key) DO UPDATE 
-		SET value = EXCLUDED.value
-	`
-	if _, err := s.db.ExecContext(ctx, stmt, upsert.UserID, upsert.Key, upsert.Value); err != nil {
+	userSetting, err := s.driver.UpsertUserSetting(ctx, upsert)
+	if err != nil {
 		return nil, err
 	}
 
-	userSetting := upsert
 	s.userSettingCache.Store(getUserSettingCacheKey(userSetting.UserID, userSetting.Key), userSetting)
 	return userSetting, nil
 }
 
 func (s *Store) ListUserSettings(ctx context.Context, find *FindUserSetting) ([]*UserSetting, error) {
-	where, args := []string{"1 = 1"}, []any{}
-
-	if v := find.Key; v != "" {
-		where, args = append(where, "key = ?"), append(args, v)
-	}
-	if v := find.UserID; v != nil {
-		where, args = append(where, "user_id = ?"), append(args, *find.UserID)
-	}
-
-	query := `
-		SELECT
-			user_id,
-			key,
-			value
-		FROM user_setting
-		WHERE ` + strings.Join(where, " AND ")
-	rows, err := s.db.QueryContext(ctx, query, args...)
+	userSettingList, err := s.driver.ListUserSettings(ctx, find)
 	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	userSettingList := make([]*UserSetting, 0)
-	for rows.Next() {
-		var userSetting UserSetting
-		if err := rows.Scan(
-			&userSetting.UserID,
-			&userSetting.Key,
-			&userSetting.Value,
-		); err != nil {
-			return nil, err
-		}
-		userSettingList = append(userSettingList, &userSetting)
-	}
-
-	if err := rows.Err(); err != nil {
 		return nil, err
 	}
 
@@ -113,85 +67,18 @@ type FindUserSettingV1 struct {
 }
 
 func (s *Store) UpsertUserSettingV1(ctx context.Context, upsert *storepb.UserSetting) (*storepb.UserSetting, error) {
-	stmt := `
-		INSERT INTO user_setting (
-			user_id, key, value
-		)
-		VALUES (?, ?, ?)
-		ON CONFLICT(user_id, key) DO UPDATE 
-		SET value = EXCLUDED.value
-	`
-	var valueString string
-	if upsert.Key == storepb.UserSettingKey_USER_SETTING_ACCESS_TOKENS {
-		valueBytes, err := protojson.Marshal(upsert.GetAccessTokens())
-		if err != nil {
-			return nil, err
-		}
-		valueString = string(valueBytes)
-	} else {
-		return nil, errors.New("invalid user setting key")
-	}
-
-	if _, err := s.db.ExecContext(ctx, stmt, upsert.UserId, upsert.Key.String(), valueString); err != nil {
+	userSettingMessage, err := s.driver.UpsertUserSettingV1(ctx, upsert)
+	if err != nil {
 		return nil, err
 	}
 
-	userSettingMessage := upsert
 	s.userSettingCache.Store(getUserSettingV1CacheKey(userSettingMessage.UserId, userSettingMessage.Key.String()), userSettingMessage)
 	return userSettingMessage, nil
 }
 
 func (s *Store) ListUserSettingsV1(ctx context.Context, find *FindUserSettingV1) ([]*storepb.UserSetting, error) {
-	where, args := []string{"1 = 1"}, []any{}
-
-	if v := find.Key; v != storepb.UserSettingKey_USER_SETTING_KEY_UNSPECIFIED {
-		where, args = append(where, "key = ?"), append(args, v.String())
-	}
-	if v := find.UserID; v != nil {
-		where, args = append(where, "user_id = ?"), append(args, *find.UserID)
-	}
-
-	query := `
-		SELECT
-			user_id,
-		  key,
-			value
-		FROM user_setting
-		WHERE ` + strings.Join(where, " AND ")
-	rows, err := s.db.QueryContext(ctx, query, args...)
+	userSettingList, err := s.driver.ListUserSettingsV1(ctx, find)
 	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	userSettingList := make([]*storepb.UserSetting, 0)
-	for rows.Next() {
-		userSetting := &storepb.UserSetting{}
-		var keyString, valueString string
-		if err := rows.Scan(
-			&userSetting.UserId,
-			&keyString,
-			&valueString,
-		); err != nil {
-			return nil, err
-		}
-		userSetting.Key = storepb.UserSettingKey(storepb.UserSettingKey_value[keyString])
-		if userSetting.Key == storepb.UserSettingKey_USER_SETTING_ACCESS_TOKENS {
-			accessTokensUserSetting := &storepb.AccessTokensUserSetting{}
-			if err := protojson.Unmarshal([]byte(valueString), accessTokensUserSetting); err != nil {
-				return nil, err
-			}
-			userSetting.Value = &storepb.UserSetting_AccessTokens{
-				AccessTokens: accessTokensUserSetting,
-			}
-		} else {
-			// Skip unknown user setting v1 key.
-			continue
-		}
-		userSettingList = append(userSettingList, userSetting)
-	}
-
-	if err := rows.Err(); err != nil {
 		return nil, err
 	}
 
