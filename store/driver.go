5024007fb9d2b60c84d97b2ccee2c89392ef56c6
Athurg Feng <athurg@gooth.org>
2023-09-26 14:57:04 +0800

    Move SQL code of SystemSetting into Driver

--- store/driver.go
+++ store/driver.go
@@ -4,4 +4,7 @@ import "context"
 
 type Driver interface {
 	CreateActivity(ctx context.Context, create *Activity) (*Activity, error)
+
+	UpsertSystemSetting(ctx context.Context, upsert *SystemSetting) (*SystemSetting, error)
+	ListSystemSettings(ctx context.Context, find *FindSystemSetting) ([]*SystemSetting, error)
 }
