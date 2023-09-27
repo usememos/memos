acbde4fb2d091c784457325016a11ae8e3989922
Athurg Gooth <athurg@gooth.org>
2023-09-27 08:43:46 +0800

    chore: move SQL code of MemoRelation into Driver (#2290)

--- store/memo_relation.go
+++ store/memo_relation.go
@@ -3,7 +3,6 @@ package store
 import (
 	"context"
 	"database/sql"
-	"strings"
 )
 
 type MemoRelationType string
@@ -32,77 +31,11 @@ type DeleteMemoRelation struct {
 }
 
 func (s *Store) UpsertMemoRelation(ctx context.Context, create *MemoRelation) (*MemoRelation, error) {
-	stmt := `
-		INSERT INTO memo_relation (
-			memo_id,
-			related_memo_id,
-			type
-		)
-		VALUES (?, ?, ?)
-		ON CONFLICT (memo_id, related_memo_id, type) DO UPDATE SET
-			type = EXCLUDED.type
-		RETURNING memo_id, related_memo_id, type
-	`
-	memoRelation := &MemoRelation{}
-	if err := s.db.QueryRowContext(
-		ctx,
-		stmt,
-		create.MemoID,
-		create.RelatedMemoID,
-		create.Type,
-	).Scan(
-		&memoRelation.MemoID,
-		&memoRelation.RelatedMemoID,
-		&memoRelation.Type,
-	); err != nil {
-		return nil, err
-	}
-
-	return memoRelation, nil
+	return s.driver.UpsertMemoRelation(ctx, create)
 }
 
 func (s *Store) ListMemoRelations(ctx context.Context, find *FindMemoRelation) ([]*MemoRelation, error) {
-	where, args := []string{"TRUE"}, []any{}
-	if find.MemoID != nil {
-		where, args = append(where, "memo_id = ?"), append(args, find.MemoID)
-	}
-	if find.RelatedMemoID != nil {
-		where, args = append(where, "related_memo_id = ?"), append(args, find.RelatedMemoID)
-	}
-	if find.Type != nil {
-		where, args = append(where, "type = ?"), append(args, find.Type)
-	}
-
-	rows, err := s.db.QueryContext(ctx, `
-		SELECT
-			memo_id,
-			related_memo_id,
-			type
-		FROM memo_relation
-		WHERE `+strings.Join(where, " AND "), args...)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	list := []*MemoRelation{}
-	for rows.Next() {
-		memoRelation := &MemoRelation{}
-		if err := rows.Scan(
-			&memoRelation.MemoID,
-			&memoRelation.RelatedMemoID,
-			&memoRelation.Type,
-		); err != nil {
-			return nil, err
-		}
-		list = append(list, memoRelation)
-	}
-
-	if err := rows.Err(); err != nil {
-		return nil, err
-	}
-
-	return list, nil
+	return s.driver.ListMemoRelations(ctx, find)
 }
 
 func (s *Store) GetMemoRelation(ctx context.Context, find *FindMemoRelation) (*MemoRelation, error) {
@@ -119,27 +52,7 @@ func (s *Store) GetMemoRelation(ctx context.Context, find *FindMemoRelation) (*M
 }
 
 func (s *Store) DeleteMemoRelation(ctx context.Context, delete *DeleteMemoRelation) error {
-	where, args := []string{"TRUE"}, []any{}
-	if delete.MemoID != nil {
-		where, args = append(where, "memo_id = ?"), append(args, delete.MemoID)
-	}
-	if delete.RelatedMemoID != nil {
-		where, args = append(where, "related_memo_id = ?"), append(args, delete.RelatedMemoID)
-	}
-	if delete.Type != nil {
-		where, args = append(where, "type = ?"), append(args, delete.Type)
-	}
-	stmt := `
-		DELETE FROM memo_relation
-		WHERE ` + strings.Join(where, " AND ")
-	result, err := s.db.ExecContext(ctx, stmt, args...)
-	if err != nil {
-		return err
-	}
-	if _, err = result.RowsAffected(); err != nil {
-		return err
-	}
-	return nil
+	return s.driver.DeleteMemoRelation(ctx, delete)
 }
 
 func vacuumMemoRelations(ctx context.Context, tx *sql.Tx) error {
