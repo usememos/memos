55e0fbf24e2177095f487d38b4dbcb870990616d
Athurg Gooth <athurg@gooth.org>
2023-10-08 18:29:22 +0800

    fix: create activity without some attributes  (#2356)

--- store/db/sqlite/activity.go
+++ store/db/sqlite/activity.go
@@ -2,22 +2,30 @@ package sqlite
 
 import (
 	"context"
+	"strings"
 
 	"github.com/usememos/memos/store"
 )
 
 func (d *DB) CreateActivity(ctx context.Context, create *store.Activity) (*store.Activity, error) {
-	stmt := `
-		INSERT INTO activity (
-			creator_id,
-			type,
-			level,
-			payload
-		)
-		VALUES (?, ?, ?, ?)
-		RETURNING id, created_ts
-	`
-	if err := d.db.QueryRowContext(ctx, stmt, create.CreatorID, create.Type, create.Level, create.Payload).Scan(
+	fields := []string{"`creator_id`", "`type`", "`level`", "`payload`"}
+	placeholder := []string{"?", "?", "?", "?"}
+	args := []any{create.CreatorID, create.Type, create.Level, create.Payload}
+
+	if create.ID != 0 {
+		fields = append(fields, "`id`")
+		placeholder = append(placeholder, "?")
+		args = append(args, create.ID)
+	}
+
+	if create.CreatedTs != 0 {
+		fields = append(fields, "`created_ts`")
+		placeholder = append(placeholder, "?")
+		args = append(args, create.CreatedTs)
+	}
+
+	stmt := "INSERT INTO activity (" + strings.Join(fields, ", ") + ") VALUES (" + strings.Join(placeholder, ", ") + ") RETURNING `id`, `created_ts`"
+	if err := d.db.QueryRowContext(ctx, stmt, args...).Scan(
 		&create.ID,
 		&create.CreatedTs,
 	); err != nil {
