865cc997a4d52ecfaeffb2af6c58c8c4ba207303
Steven <stevenlgtm@gmail.com>
2023-11-06 20:51:59 +0800

    chore: remove upgrade version banner

--- web/src/helpers/api.ts
+++ web/src/helpers/api.ts
@@ -179,13 +179,3 @@ export function patchIdentityProvider(identityProviderPatch: IdentityProviderPat
 export function deleteIdentityProvider(id: IdentityProviderId) {
   return axios.delete(`/api/v1/idp/${id}`);
 }
-
-export async function getRepoLatestTag() {
-  const { data } = await axios.get(`https://api.github.com/repos/usememos/memos/tags`, {
-    headers: {
-      Accept: "application/vnd.github.v3.star+json",
-      Authorization: "",
-    },
-  });
-  return data[0].name as string;
-}
