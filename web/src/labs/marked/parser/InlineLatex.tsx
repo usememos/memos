2d34615eac75aab7a017b240ab17bd87b30cd1a5
zty <tianyuan233.zhang@gmail.com>
2023-11-18 12:41:33 +0800

    fix:  parse inline latex as a inline element (#2525)
    * fix:  parse inline latex as a inline element
    
    * Update web/src/labs/marked/parser/InlineLatex.tsx
    
    ---------
    
    Co-authored-by: zty <zty.dev@outlook.com>
    Co-authored-by: boojack <stevenlgtm@gmail.com>

--- web/src/labs/marked/parser/InlineLatex.tsx
+++ web/src/labs/marked/parser/InlineLatex.tsx
@@ -12,11 +12,9 @@ const inlineRenderer = (rawStr: string) => {
     } else if (matchResult[2]) {
       latexCode = matchResult[2];
     }
-    return (
-      <div className="w-full max-w-full overflow-x-auto">
+    return <div className="max-w-full overflow-x-auto">
         <TeX key={latexCode}>{latexCode}</TeX>
       </div>
-    );
   }
   return rawStr;
 };
