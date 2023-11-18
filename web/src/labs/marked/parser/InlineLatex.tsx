18f8e39b50b37332e90ab41f63444aa5f93ee927
boojack <stevenlgtm@gmail.com>
2023-11-18 12:41:21 +0800

    Update web/src/labs/marked/parser/InlineLatex.tsx

--- web/src/labs/marked/parser/InlineLatex.tsx
+++ web/src/labs/marked/parser/InlineLatex.tsx
@@ -12,7 +12,9 @@ const inlineRenderer = (rawStr: string) => {
     } else if (matchResult[2]) {
       latexCode = matchResult[2];
     }
-    return <TeX key={latexCode}>{latexCode}</TeX>;
+    return <div className="max-w-full overflow-x-auto">
+        <TeX key={latexCode}>{latexCode}</TeX>
+      </div>
   }
   return rawStr;
 };
