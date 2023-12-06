055a327b5eac618bd9013f9afaa6203546d8325c
t.yang <102474041+Tyangs@users.noreply.github.com>
2023-12-06 10:58:33 +0800

    fix: detail page user-avatar size have unexpected height (#2576)
    Co-authored-by: tyangs.yang <tyangs.yang@hstong.com>

--- web/src/pages/MemoDetail.tsx
+++ web/src/pages/MemoDetail.tsx
@@ -114,7 +114,7 @@ const MemoDetail = () => {
       <section className="relative top-0 w-full min-h-full overflow-x-hidden bg-zinc-100 dark:bg-zinc-900">
         <div className="relative w-full h-auto mx-auto flex flex-col justify-start items-center bg-white dark:bg-zinc-700">
           <div className="w-full flex flex-col justify-start items-center pt-16 pb-8">
-            <UserAvatar className="!w-20 h-20 mb-2 drop-shadow" avatarUrl={systemStatus.customizedProfile.logoUrl} />
+            <UserAvatar className="!w-20 !h-20 mb-2 drop-shadow" avatarUrl={systemStatus.customizedProfile.logoUrl} />
             <p className="text-3xl text-black opacity-80 dark:text-gray-200">{systemStatus.customizedProfile.name}</p>
           </div>
           <div className="relative flex-grow max-w-2xl w-full min-h-full flex flex-col justify-start items-start px-4 pb-6">
