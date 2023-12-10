9f4f2e8e27651ef34dbaba2b527b102a45522530
Steven <stevenlgtm@gmail.com>
2023-12-10 11:57:31 +0800

    chore: fix user setting values

--- store/db/mysql/migration/prod/0.18/02__user_setting.sql
+++ store/db/mysql/migration/prod/0.18/02__user_setting.sql
@@ -1,4 +1,4 @@
-UPDATE `user_setting` SET `key` = 'USER_SETTING_LOCALE' WHERE `key` = 'locale';
-UPDATE `user_setting` SET `key` = 'USER_SETTING_APPEARANCE' WHERE `key` = 'appearance';
-UPDATE `user_setting` SET `key` = 'USER_SETTING_MEMO_VISIBILITY' WHERE `key` = 'memo-visibility';
-UPDATE `user_setting` SET `key` = 'USER_SETTING_TELEGRAM_USER_ID' WHERE `key` = 'telegram-user-id';
+UPDATE `user_setting` SET `key` = 'USER_SETTING_LOCALE', `value` = REPLACE(`value`, '"', '') WHERE `key` = 'locale';
+UPDATE `user_setting` SET `key` = 'USER_SETTING_APPEARANCE', `value` = REPLACE(`value`, '"', '') WHERE `key` = 'appearance';
+UPDATE `user_setting` SET `key` = 'USER_SETTING_MEMO_VISIBILITY', `value` = REPLACE(`value`, '"', '') WHERE `key` = 'memo-visibility';
+UPDATE `user_setting` SET `key` = 'USER_SETTING_TELEGRAM_USER_ID', `value` = REPLACE(`value`, '"', '') WHERE `key` = 'telegram-user-id';
