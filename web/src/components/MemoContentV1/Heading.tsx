0793f965787e011ffca060de536dae2aa39aa243
Steven <stevenlgtm@gmail.com>
2023-12-17 11:08:13 +0800

    chore: update heading styles

--- web/src/components/MemoContentV1/Heading.tsx
+++ web/src/components/MemoContentV1/Heading.tsx
@@ -8,8 +8,21 @@ interface Props {
 
 const Heading: React.FC<Props> = ({ level, children }: Props) => {
   const Head = `h${level}` as keyof JSX.IntrinsicElements;
+  const className = (() => {
+    switch (level) {
+      case 1:
+        return "text-5xl leading-normal font-bold";
+      case 2:
+        return "text-3xl leading-normal font-medium";
+      case 3:
+        return "text-xl leading-normal font-medium";
+      case 4:
+        return "text-lg font-bold";
+    }
+  })();
+
   return (
-    <Head>
+    <Head className={className}>
       {children.map((child, index) => (
         <Renderer key={`${child.type}-${index}`} node={child} />
       ))}
