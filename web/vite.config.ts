be2db3f1700c0fd99eec18f65703ad032e777454
Leyang <quanruzhuoxiu@gmail.com>
2023-12-23 12:00:03 +0800

    feat: use vite plugin pwa for generate right sw.js (#2658)
    Co-authored-by: boojack <stevenlgtm@gmail.com>

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,7 @@
 import react from "@vitejs/plugin-react-swc";
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 let devProxyServer = "http://localhost:8081/";
 if (process.env.DEV_PROXY_SERVER && process.env.DEV_PROXY_SERVER.length > 0) {
@@ -10,7 +11,49 @@ if (process.env.DEV_PROXY_SERVER && process.env.DEV_PROXY_SERVER.length > 0) {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifestFilename: "manifest.json",
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        short_name: "memos",
+        name: "memos",
+        description: "usememos/memos",
+        start_url: "/",
+        scope: "/",
+        display: "standalone",
+        theme_color: "#f4f4f5",
+        background_color: "#f4f4f5",
+        icons: [
+          {
+            src: "/logo.png",
+            type: "image/png",
+            sizes: "16x16",
+          },
+          {
+            src: "/logo.png",
+            type: "image/png",
+            sizes: "32x32",
+          },
+          {
+            src: "/logo.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/logo.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     host: "0.0.0.0",
     port: 3001,
