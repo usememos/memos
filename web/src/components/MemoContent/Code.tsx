c9146bc749b28443144413d0e105e543352b9d91
Steven <stevenlgtm@gmail.com>
2024-01-19 07:06:28 +0800

    chore: update code style

--- web/src/components/MemoContent/Code.tsx
+++ web/src/components/MemoContent/Code.tsx
@@ -3,7 +3,7 @@ interface Props {
 }
 
 const Code: React.FC<Props> = ({ content }: Props) => {
-  return <code className="text-sm">`{content}`</code>;
+  return <code className="inline break-all px-1 py-0.5 leading-none text-sm rounded bg-gray-100 dark:bg-zinc-700">{content}</code>;
 };
 
 export default Code;
