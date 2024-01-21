54a24833a794ac13ec0241e1e66a613275197d62
Steven <stevenlgtm@gmail.com>
2024-01-21 21:36:31 +0800

    chore: fix resource seeds

--- store/db/sqlite/seed/10006__resource.sql
+++ store/db/sqlite/seed/10006__resource.sql
@@ -1,4 +1,4 @@
 INSERT INTO
-  resource (`creator_id`, `filename`, `external_link`, `type`, `memo_id`)
+  resource (`resource_name`, `creator_id`, `filename`, `external_link`, `type`, `memo_id`)
 VALUES
-  (101, 'slash-demo.png', 'https://github.com/yourselfhosted/slash/blob/main/docs/assets/demo.png?raw=true', 'image/png', 3);
+  ("slash-demo", 101, 'slash-demo.png', 'https://github.com/yourselfhosted/slash/blob/main/docs/assets/demo.png?raw=true', 'image/png', 3);
