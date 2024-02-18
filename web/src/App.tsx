e010a9d70a596e7a6e053986ff89041f2d678f2a
Kristian <kpskola1002@gmail.com>
2024-02-18 11:34:14 +0100

    feat: allow automatci mermaid theme swithcing based on global theme selected

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -19,7 +19,7 @@ const App = () => {
   const { appearance, locale, systemStatus } = globalStore.state;
   const userSetting = userStore.userSetting;
 
-  mermaid.initialize({ startOnLoad: false, theme: "dark" });
+  mermaid.initialize({ startOnLoad: false, theme: mode });
 
   // Redirect to sign up page if no host.
   useEffect(() => {
