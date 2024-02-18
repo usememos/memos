09e9ff3a705a5a97101fa528e1be2bf49a17db61
Kristian <kpskola1002@gmail.com>
2024-02-18 11:35:04 +0100

    refactor: make render block to directly render

--- web/src/components/MemoContent/CodeBlock.tsx
+++ web/src/components/MemoContent/CodeBlock.tsx
@@ -15,13 +15,15 @@ interface Props extends BaseProps {
 
 const CodeBlock: React.FC<Props> = ({ language, content }: Props) => {
   const formatedLanguage = (language || "").toLowerCase() || "text";
-  let languageSpecificClasses = "";
-
   let highlightedCode = content;
 
   // Users can set Markdown code blocks as `__html` to render HTML directly.
   if (formatedLanguage === "__html") {
     return <div className="w-full overflow-auto !my-2" dangerouslySetInnerHTML={{ __html: content }} />;
+  }
+
+  if (formatedLanguage === MERMAID_LANGUAGE) {
+    return <MermaidBlock __html={highlightedCode} />;
   }
 
   try {
@@ -33,35 +35,25 @@ const CodeBlock: React.FC<Props> = ({ language, content }: Props) => {
     // Skip error and use default highlighted code.
   }
 
-  if (formatedLanguage === MERMAID_LANGUAGE) {
-    languageSpecificClasses = "mermaid";
-  }
-
   const handleCopyButtonClick = () => {
     copy(content);
     toast.success("Copied to clipboard!");
   };
 
-  return languageSpecificClasses !== MERMAID_LANGUAGE ? (
+  return (
     <div className="w-full my-1 bg-amber-100 border-l-4 border-amber-400 rounded overflow-clip hover:shadow dark:bg-zinc-600 dark:border-zinc-400">
       <div className="w-full px-2 py-1 flex flex-row justify-between items-center text-amber-500 dark:text-zinc-400">
         <span className="text-sm font-mono">{formatedLanguage}</span>
         <Icon.Copy className="w-4 h-auto cursor-pointer hover:opacity-80" onClick={handleCopyButtonClick} />
       </div>
 
-      <pre className={classNames("w-full p-2 bg-amber-50 dark:bg-zinc-700 whitespace-pre-wrap relative", languageSpecificClasses)}>
+      <pre className="w-full p-2 bg-amber-50 dark:bg-zinc-700 whitespace-pre-wrap relative">
         <code
           className={classNames(`language-${formatedLanguage}`, "block text-sm leading-5")}
           dangerouslySetInnerHTML={{ __html: highlightedCode }}
         ></code>
       </pre>
     </div>
-  ) : (
-    <MermaidBlock
-      index="mermaid"
-      __html={highlightedCode}
-      className={classNames("w-full p-2 whitespace-pre-wrap relative", languageSpecificClasses)}
-    />
   );
 };
 
