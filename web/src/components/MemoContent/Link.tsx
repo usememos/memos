edbb22aab50408f09918d8f6e3dd817bf6def896
MehadND <mehadnd@gmail.com>
2024-03-06 02:16:14 -0800

    chore: fixed linter errors and added support for ossible future translations

--- web/src/components/MemoContent/Link.tsx
+++ web/src/components/MemoContent/Link.tsx
@@ -1,7 +1,7 @@
-import { useTranslate } from "@/utils/i18n";
-import { AspectRatio, Box, Button, Card, Tooltip } from "@mui/joy";
+import { AspectRatio, Box, Card, Tooltip } from "@mui/joy";
 import { Link as MLink } from "@mui/joy";
 import { useState, useEffect } from "react";
+import { useTranslate } from "@/utils/i18n";
 
 interface Props {
   url: string;
@@ -36,64 +36,64 @@ const Link: React.FC<Props> = ({ text, url }: Props) => {
 
   return (
     <>
-    {/* Small screens */}
-    <div className="flex md:hidden">
-      <MLink href={url} underline="none" sx={{ fontWeight: "lg" }}>
-        {url || text}
-      </MLink>
-    </div>
+      {/* Small screens */}
+      <div className="flex md:hidden">
+        <MLink href={url} underline="none" sx={{ fontWeight: "lg" }}>
+          {url || text}
+        </MLink>
+      </div>
 
-    {/* Medium & Above screens */}
-    <div className="hidden md:flex">
-    <Tooltip
-      placement="top-end"
-      variant="solid"
-      sx={{}}
-      title={
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            maxWidth: 450,
-            maxHeight: 300,
-          }}
-        >
-          {image ? (
-            <a href={url} target="_blank" rel="noopener noreferrer" className="flex w-full">
-              <Card variant="outlined" orientation="vertical" sx={{ width: "100%" }}>
-                <AspectRatio ratio={"21/9"} objectFit="cover" variant="plain">
-                  <img src={image} alt={title} className="pointer-events-none" />
-                </AspectRatio>
-                <div className="flex-1 overflow-auto w-full">
-                  <div className="flex flex-col justify-between ">
-                    <div>
-                      <h3 className="text-2xl font-semibold tracking-tight truncate">{title}</h3>
-                      <p className="text-sm truncate">{description}</p>
+      {/* Medium & Above screens */}
+      <div className="hidden md:flex">
+        <Tooltip
+          placement="top-end"
+          variant="solid"
+          sx={{}}
+          title={
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                maxWidth: 450,
+                maxHeight: 300,
+              }}
+            >
+              {image ? (
+                <a href={url} target="_blank" rel="noopener noreferrer" className="flex w-full">
+                  <Card variant="outlined" orientation="vertical" sx={{ width: "100%" }}>
+                    <AspectRatio ratio={"21/9"} objectFit="cover" variant="plain">
+                      <img src={image} alt={title} className="pointer-events-none" />
+                    </AspectRatio>
+                    <div className="flex-1 overflow-auto w-full">
+                      <div className="flex flex-col justify-between ">
+                        <div>
+                          <h3 className="text-2xl font-semibold tracking-tight truncate">{title}</h3>
+                          <p className="text-sm truncate">{description}</p>
+                        </div>
+                      </div>
+                    </div>
+                  </Card>
+                </a>
+              ) : (
+                <Card variant="soft" orientation="vertical" sx={{ width: "100%" }}>
+                  <div className="flex-1 overflow-auto w-full">
+                    <div className="flex flex-col justify-between ">
+                      <div>
+                        <h3 className="text-2xl font-semibold tracking-tight">{title}</h3>
+                        <p className="text-sm text-black/50 dark:text-white/50">{t("common.no-preview")}</p>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </Card>
-            </a>
-          ) : (
-            <Card variant="soft" orientation="vertical" sx={{ width: "100%" }}>
-              <div className="flex-1 overflow-auto w-full">
-                <div className="flex flex-col justify-between ">
-                  <div>
-                    <h3 className="text-2xl font-semibold tracking-tight">{title}</h3>
-                    <p className="text-sm text-black/50 dark:text-white/50">{t("common.no-preview")}</p>
-                  </div>
-                </div>
-              </div>
-            </Card>
-          )}
-        </Box>
-      }
-    >
-      <MLink href={url} underline="none" sx={{ fontWeight: "lg" }}>
-        {url || text}
-      </MLink>
-    </Tooltip>
-    </div>
+                </Card>
+              )}
+            </Box>
+          }
+        >
+          <MLink href={url} underline="none" sx={{ fontWeight: "lg" }}>
+            {url || text}
+          </MLink>
+        </Tooltip>
+      </div>
     </>
   );
 };
