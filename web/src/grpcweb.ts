d01800d32bce94cd9797401c9d014d029b4a92f2
MehadND <mehadnd@gmail.com>
2024-03-13 13:51:20 +0500

    chore: updated frontend gen types with the renamed (server) service file

--- web/src/grpcweb.ts
+++ web/src/grpcweb.ts
@@ -3,7 +3,7 @@ import { ActivityServiceDefinition } from "./types/proto/api/v2/activity_service
 import { AuthServiceDefinition } from "./types/proto/api/v2/auth_service";
 import { InboxServiceDefinition } from "./types/proto/api/v2/inbox_service";
 import { MemoServiceDefinition } from "./types/proto/api/v2/memo_service";
-import { LinkServiceDefinition } from "./types/proto/api/v2/metadata_service";
+import { LinkServiceDefinition } from "./types/proto/api/v2/link_service";
 import { ResourceServiceDefinition } from "./types/proto/api/v2/resource_service";
 import { TagServiceDefinition } from "./types/proto/api/v2/tag_service";
 import { UserServiceDefinition } from "./types/proto/api/v2/user_service";
