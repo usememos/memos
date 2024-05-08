33133ea1a3a0112bf42486e5421ccc1dffe452c1
Steven <stevenlgtm@gmail.com>
2024-05-08 22:50:13 +0800

    chore: tweak searchbar styles

--- web/src/components/SearchBar.tsx
+++ web/src/components/SearchBar.tsx
@@ -30,7 +30,7 @@ const SearchBar = () => {
     <div className="relative w-full h-auto flex flex-row justify-start items-center">
       <Icon.Search className="absolute left-3 w-4 h-auto opacity-30" />
       <input
-        className="w-full text-gray-500 dark:text-gray-400 bg-zinc-50 dark:bg-zinc-900 border dark:border-zinc-800 text-sm leading-6 rounded-lg p-1 pl-8 outline-none"
+        className="w-full text-gray-500 dark:text-gray-400 bg-zinc-50 dark:bg-zinc-900 border dark:border-zinc-800 text-sm leading-7 rounded-lg p-1 pl-8 outline-none"
         placeholder={t("memo.search-placeholder")}
         value={queryText}
         onChange={handleTextQueryInput}
