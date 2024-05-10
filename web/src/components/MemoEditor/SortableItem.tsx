d0be288a6156d23a2224ad441361f54cfdb3e56b
boojack <stevenlgtm@gmail.com>
2024-05-10 19:48:57 +0800

    Implement drag and drop for resource order in editor

---
+++ web/src/components/MemoEditor/SortableItem.tsx
@@ -0,0 +1,32 @@
+import { memo } from 'react';
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
+
+interface SortableItemProps {
+  id: string;
+  className: string;
+  children: React.ReactNode;
+}
+
+const SortableItem: React.FC<SortableItemProps> = memo(({ id, className, children }) => {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+  } = useSortable({ id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
+  return (
+    <div ref={setNodeRef} style={style} {...attributes} {...listeners} className={className}>
+      {children}
+    </div>
+  );
+});
+
+export default SortableItem;
