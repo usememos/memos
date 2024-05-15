cc4512f1f8e1697a4162c60f8b5b3d3e551efeff
wzc90 <108047478+wzc90@users.noreply.github.com>
2024-05-15 17:25:59 +0800

    fix: migration scripts for mysql

--- store/db/mysql/migration/prod/0.22/00__resource_storage_type.sql
+++ store/db/mysql/migration/prod/0.22/00__resource_storage_type.sql
@@ -2,6 +2,8 @@ ALTER TABLE `resource` ADD COLUMN `storage_type` VARCHAR(256) NOT NULL DEFAULT '
 ALTER TABLE `resource` ADD COLUMN `reference` VARCHAR(256) NOT NULL DEFAULT '';
 ALTER TABLE `resource` ADD COLUMN `payload` TEXT NOT NULL;
 
+UPDATE `resource` SET `payload` = '{}';
+
 UPDATE `resource` SET `storage_type` = 'LOCAL', `reference` = `internal_path` WHERE `internal_path` IS NOT NULL AND `internal_path` != '';
 UPDATE `resource` SET `storage_type` = 'EXTERNAL', `reference` = `external_link` WHERE `external_link` IS NOT NULL AND `external_link` != '';
 
