2ea37fbbd97403641088604a2a1c31b4627d5b81
Ryo <36154873+RyoJerryYu@users.noreply.github.com>
2024-06-10 00:37:18 +0800

    feat: return not found instead of internal for resource

--- server/router/api/v1/resource_service.go
+++ server/router/api/v1/resource_service.go
@@ -197,6 +197,9 @@ func (s *APIV1Service) GetResourceBinary(ctx context.Context, request *v1pb.GetR
 		}
 
 		file, err := os.Open(resourcePath)
+		if os.IsNotExist(err) {
+			return nil, status.Errorf(codes.NotFound, "file not found for resource: %s", request.Name)
+		}
 		if err != nil {
 			return nil, status.Errorf(codes.Internal, "failed to open the file: %v", err)
 		}
