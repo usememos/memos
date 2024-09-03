9612c302c4e897057967112be37f1d4adc0c061c
Steven <stevenlgtm@gmail.com>
2024-09-03 22:17:46 +0800

    chore: bump version to `v0.22.5`

--- server/version/version.go
+++ server/version/version.go
@@ -9,10 +9,10 @@ import (
 
 // Version is the service current released version.
 // Semantic versioning: https://semver.org/
-var Version = "0.22.4"
+var Version = "0.22.5"
 
 // DevVersion is the service current development version.
-var DevVersion = "0.22.4"
+var DevVersion = "0.22.5"
 
 func GetCurrentVersion(mode string) string {
 	if mode == "dev" || mode == "demo" {
