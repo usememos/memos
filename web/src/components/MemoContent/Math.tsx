2ebf3f26151731a7e751a26a085484c801a1874e
johnnyjoy <yourselfhosted@gmail.com>
2024-10-19 21:40:24 +0800

    chore: fix math styles

--- web/src/components/MemoContent/Math.tsx
+++ web/src/components/MemoContent/Math.tsx
@@ -1,4 +1,5 @@
 import TeX from "@matejmazur/react-katex";
+import clsx from "clsx";
 import "katex/dist/katex.min.css";
 
 interface Props {
@@ -7,7 +8,7 @@ interface Props {
 }
 
 const Math: React.FC<Props> = ({ content, block }: Props) => {
-  return <TeX className="max-w-full inline-block overflow-auto" block={block} math={content}></TeX>;
+  return <TeX className={clsx("max-w-full", block ? "w-full block" : "inline text-sm")} block={block} math={content}></TeX>;
 };
 
 export default Math;
