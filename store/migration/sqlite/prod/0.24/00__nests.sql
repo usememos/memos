96c6e1836d61701bdf0c7dc297d1f68556b4ef7b
Andrés MANELLI <am@toroid.io>
2024-11-14 01:39:01 +0100

    feat(nest): fix create table for migrations

--- store/migration/sqlite/prod/0.24/00__nests.sql
+++ store/migration/sqlite/prod/0.24/00__nests.sql
@@ -3,6 +3,14 @@ ALTER TABLE
 ADD
   COLUMN nest INTEGER NOT NULL DEFAULT 0;
 
+CREATE TABLE nest (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  creator_id INTEGER NOT NULL,
+  created_ts BIGINT NOT NULL DEFAULT (strftime('%s', 'now')),
+  row_status TEXT NOT NULL CHECK (row_status IN ('NORMAL', 'ARCHIVED')) DEFAULT 'NORMAL'
+);
+
 INSERT INTO
   nest (name, creator_id)
 SELECT
