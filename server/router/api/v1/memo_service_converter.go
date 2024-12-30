7817ad07f7a4e59784d8648789c34c4f7263519a
johnnyjoy <yourselfhosted@gmail.com>
2024-12-30 09:18:11 +0800

    chore: fix linter

--- server/router/api/v1/memo_service_converter.go
+++ server/router/api/v1/memo_service_converter.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/usememos/gomark/parser"
 	"github.com/usememos/gomark/parser/tokenizer"
+
 	v1pb "github.com/usememos/memos/proto/gen/api/v1"
 	storepb "github.com/usememos/memos/proto/gen/store"
 	"github.com/usememos/memos/store"
