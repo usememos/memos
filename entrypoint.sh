82b5c8ab0795fe98026142eadbcee3f225997de0
Hadley Rich <hads@nice.net.nz>
2024-12-30 22:46:02 +1300

    feat: add sh entrypoint to allow MEMOS_DSN_FILE to load variable from secret (#4236)
    Add sh entrypoint to allow MEMOS_DSN_FILE to load variable from secret

---
+++ entrypoint.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+file_env() {
+   var="$1"
+   fileVar="${var}_FILE"
+
+   val_var="$(printenv "$var")"
+   val_fileVar="$(printenv "$fileVar")"
+
+   if [ -n "$val_var" ] && [ -n "$val_fileVar" ]; then
+      echo "error: both $var and $fileVar are set (but are exclusive)" >&2
+      exit 1
+   fi
+
+   if [ -n "$val_var" ]; then
+      val="$val_var"
+   elif [ -n "$val_fileVar" ]; then
+      val="$(cat "$val_fileVar")"
+   fi
+
+   export "$var"="$val"
+   unset "$fileVar"
+}
+
+file_env "MEMOS_DSN"
+
+exec "$@"
