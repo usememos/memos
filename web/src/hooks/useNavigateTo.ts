d81174ad7c0a8e44c48ad3e2fd7803690287cb52
Chris Curry <hichriscurry@gmail.com>
2025-01-04 23:42:49 +0800

    feat: optimize filters sync (#4260)
    * refactor: add bi-directional filters sync between filterStore and searchParams
    
    * fix: tag redirection from memos detail page, https://github.com/usememos/memos/issues/4232

--- web/src/hooks/useNavigateTo.ts
+++ web/src/hooks/useNavigateTo.ts
@@ -1,15 +1,15 @@
-import { useNavigate } from "react-router-dom";
+import { NavigateOptions, useNavigate } from "react-router-dom";
 
 const useNavigateTo = () => {
   const navigateTo = useNavigate();
 
-  const navigateToWithViewTransition = (to: string) => {
+  const navigateToWithViewTransition = (to: string, options?: NavigateOptions) => {
     const document = window.document as any;
     if (!document.startViewTransition) {
-      navigateTo(to);
+      navigateTo(to, options);
     } else {
       document.startViewTransition(() => {
-        navigateTo(to);
+        navigateTo(to, options);
       });
     }
   };
