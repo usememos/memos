130b118df4099cca344537ef9d0748b80d51e3f7
root <root@DESKTOP-G3MCU14>
2025-01-11 19:12:48 +0530

    now mermaid block uses theme according to the system as well when 'follow system' is selected

--- web/src/components/MemoContent/MermaidBlock.tsx
+++ web/src/components/MemoContent/MermaidBlock.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from "react";
+import { getSystemColorScheme } from "@/helpers/utils";
 import { useCommonContext } from "@/layouts/CommonContextProvider";
 
 interface Props {
@@ -8,11 +9,27 @@ interface Props {
 const MermaidBlock: React.FC<Props> = ({ content }: Props) => {
   const mermaidDockBlock = useRef<null>(null);
   const commonContext = useCommonContext();
+
+  const handleMermaidTheme = () => {
+    if (commonContext.appearance === "dark") {
+      return "dark";
+    } else if (commonContext.appearance === "light") {
+      return "neutral";
+    } else {
+      if (getSystemColorScheme() === "dark") {
+        return "dark";
+      } else if (getSystemColorScheme() === "light") {
+        return "neutral";
+      } else {
+        return "default";
+      }
+    }
+  };
   useEffect(() => {
     // Dynamically import mermaid to ensure compatibility with Vite
     const initializeMermaid = async () => {
       const mermaid = (await import("mermaid")).default;
-      mermaid.initialize({ startOnLoad: false, theme: commonContext.appearance === "dark" ? "dark" : "default" });
+      mermaid.initialize({ startOnLoad: false, theme: handleMermaidTheme() });
       if (mermaidDockBlock.current) {
         mermaid.run({
           nodes: [mermaidDockBlock.current],
