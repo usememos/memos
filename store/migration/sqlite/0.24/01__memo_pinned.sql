37a81f9d28810074c0dc712296a0762792fed52e
Xiao Wei <403828237@qq.com>
2025-02-01 22:11:17 +0800

    fix: migrate sql error (#4338)
    * fix: migrate sql error
    
    sqlite not support postgres sql
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Johnny <yourselfhosted@gmail.com>

--- store/migration/sqlite/0.24/01__memo_pinned.sql
+++ store/migration/sqlite/0.24/01__memo_pinned.sql
@@ -4,5 +4,8 @@ ALTER TABLE memo ADD COLUMN pinned INTEGER NOT NULL CHECK (pinned IN (0, 1)) DEF
 -- Update pinned column from memo_organizer.
 UPDATE memo
 SET pinned = 1
-FROM memo_organizer
-WHERE memo.id = memo_organizer.memo_id AND memo_organizer.pinned = 1;
+WHERE EXISTS (
+    SELECT 1
+    FROM memo_organizer
+    WHERE memo.id = memo_organizer.memo_id AND memo_organizer.pinned = 1
+);
