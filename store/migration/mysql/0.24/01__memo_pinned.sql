6b4df0b09775db2e7468ebc8fc8528d785f3e6b7
wzc90 <108047478+wzc90@users.noreply.github.com>
2025-02-06 07:43:33 +0800

    fix Update 01__memo_pinned.sql

--- store/migration/mysql/0.24/01__memo_pinned.sql
+++ store/migration/mysql/0.24/01__memo_pinned.sql
@@ -2,7 +2,7 @@
 ALTER TABLE `memo` ADD COLUMN `pinned` BOOLEAN NOT NULL DEFAULT FALSE;
 
 -- Update pinned column from memo_organizer.
-UPDATE `memo`
-SET `pinned` = TRUE
-FROM `memo_organizer`
-WHERE `memo`.`id` = `memo_organizer`.`memo_id` AND `memo_organizer`.`pinned` = 1;
+UPDATE memo
+JOIN memo_organizer ON memo.id = memo_organizer.memo_id
+SET memo.pinned = TRUE
+WHERE memo_organizer.pinned = 1;
