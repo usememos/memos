54351bf9fea39b69da93104a0037b0a50840afc7
taoxin <xinxintao@foxmail.com>
2025-02-09 23:45:11 +0800

    feat: add tab

--- web/src/components/MemoDetailSidebar/MemoDetailSidebar.tsx
+++ web/src/components/MemoDetailSidebar/MemoDetailSidebar.tsx
@@ -49,7 +49,7 @@ const MemoDetailSidebar = ({ memo, className, parentPage }: Props) => {
         {hasSpecialProperty && (
           <div className="w-full flex flex-col">
             <p className="flex flex-row justify-start items-center w-full gap-1 mb-1 text-sm leading-6 text-gray-400 dark:text-gray-500 select-none">
-            <span>{t("common.properties")}</span>
+              <span>{t("common.properties")}</span>
             </p>
             <div className="w-full flex flex-row justify-start items-center gap-x-2 gap-y-1 flex-wrap text-gray-500 dark:text-gray-400">
               {property.hasLink && (
