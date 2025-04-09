b798037fe8965cc0faa3d17b1c3e09dab235478e
Johnny <yourselfhosted@gmail.com>
2025-04-09 22:21:43 +0800

    Update server/router/frontend/frontend.go

--- server/router/frontend/frontend.go
+++ server/router/frontend/frontend.go
@@ -36,7 +36,6 @@ func (*FrontendService) Serve(_ context.Context, e *echo.Echo) {
 		}
 		// Set Cache-Control header to allow public caching with a max-age of 30 days (in seconds).
 		c.Response().Header().Set(echo.HeaderCacheControl, "public, max-age=2592000")
-		c.Response().Header().Set(echo.HeaderCacheControl, "public, max-age=2592000")
 		return false
 	}
 
