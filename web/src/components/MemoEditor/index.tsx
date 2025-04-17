b324fe173abb4d78b4ec3d449b889803be4f1423
sawatkins <sw@sawatkins.com>
2025-04-17 09:50:43 -0700

    use tailwind for cursor styles

--- web/src/components/MemoEditor/index.tsx
+++ web/src/components/MemoEditor/index.tsx
@@ -472,7 +472,7 @@ const MemoEditor = observer((props: Props) => {
         className={`${
           className ?? ""
         } relative w-full flex flex-col justify-start items-start bg-white dark:bg-zinc-800 px-4 pt-4 rounded-lg border ${
-          state.isDraggingFile ? "border-dashed border-gray-400 dark:border-primary-400" : "border-gray-200 dark:border-zinc-700"
+          state.isDraggingFile ? "border-dashed border-gray-400 dark:border-primary-400 cursor-copy" : "border-gray-200 dark:border-zinc-700 cursor-auto"
         }`}
         tabIndex={0}
         onKeyDown={handleKeyDown}
@@ -482,9 +482,6 @@ const MemoEditor = observer((props: Props) => {
         onFocus={handleEditorFocus}
         onCompositionStart={handleCompositionStart}
         onCompositionEnd={handleCompositionEnd}
-        style={{
-          cursor: state.isDraggingFile ? "copy" : "auto",
-        }}
       >
         {memoName && displayTime && (
           <DatePicker
