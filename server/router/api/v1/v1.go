712d7056a2aa029f3ccde20e55cf217be516d44e
Nathan Krieger <124911662+nathankrieger@users.noreply.github.com>
2025-05-05 10:27:10 -0500

    WIP

--- server/router/api/v1/v1.go
+++ server/router/api/v1/v1.go
@@ -55,6 +55,7 @@ func NewAPIV1Service(secret string, profile *profile.Profile, store *store.Store
 	v1pb.RegisterAuthServiceServer(grpcServer, apiv1Service)
 	v1pb.RegisterUserServiceServer(grpcServer, apiv1Service)
 	v1pb.RegisterMemoServiceServer(grpcServer, apiv1Service)
+	v1pb.RegisterMemoCommentServiceServer(grpcServer, apiv1Service)
 	v1pb.RegisterResourceServiceServer(grpcServer, apiv1Service)
 	v1pb.RegisterInboxServiceServer(grpcServer, apiv1Service)
 	v1pb.RegisterActivityServiceServer(grpcServer, apiv1Service)
