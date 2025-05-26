6964c1df054a3130cd6b24424a3d43b6c5e9700e
dabeeduu <dabeeduu+git@gmail.com>
2025-05-26 13:50:13 +0700

    fix: make the save button disabled after a successful update (#4721)
    * fix: make the save button disabled after a successful update
    
    * fix: fixing formatting error
    
    ---------
    
    Co-authored-by: didi <didi@localhost.localdomain>

--- web/src/components/Settings/MemoRelatedSettings.tsx
+++ web/src/components/Settings/MemoRelatedSettings.tsx
@@ -12,7 +12,7 @@ import { useTranslate } from "@/utils/i18n";
 
 const MemoRelatedSettings = () => {
   const t = useTranslate();
-  const originalSetting = workspaceStore.state.memoRelatedSetting;
+  const [originalSetting, setOriginalSetting] = useState<WorkspaceMemoRelatedSetting>(workspaceStore.state.memoRelatedSetting);
   const [memoRelatedSetting, setMemoRelatedSetting] = useState<WorkspaceMemoRelatedSetting>(originalSetting);
   const [editingReaction, setEditingReaction] = useState<string>("");
   const [editingNsfwTag, setEditingNsfwTag] = useState<string>("");
@@ -54,12 +54,12 @@ const MemoRelatedSettings = () => {
         name: `${workspaceSettingNamePrefix}${WorkspaceSettingKey.MEMO_RELATED}`,
         memoRelatedSetting,
       });
+      setOriginalSetting(memoRelatedSetting);
+      toast.success(t("message.update-succeed"));
     } catch (error: any) {
       toast.error(error.details);
       console.error(error);
-      return;
     }
-    toast.success(t("message.update-succeed"));
   };
 
   return (
