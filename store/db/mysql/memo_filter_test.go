f5c8c7fc90f404180f2b88e63917a6832998c416
Meer Uzair <meer.uxair007@gmail.com>
2025-05-28 21:33:04 +0530

    added test for hasIncompletTasks filter

--- store/db/mysql/memo_filter_test.go
+++ store/db/mysql/memo_filter_test.go
@@ -94,6 +94,41 @@ func TestConvertExprToSQL(t *testing.T) {
 			want:   "(JSON_EXTRACT(`memo`.`payload`, '$.property.hasTaskList') = CAST('true' AS JSON) AND `memo`.`content` LIKE ?)",
 			args:   []any{"%todo%"},
 		},
+		{
+			filter: `has_incomplete_tasks`,
+			want:   "JSON_EXTRACT(`memo`.`payload`, '$.property.hasIncompleteTasks') = CAST('true' AS JSON)",
+			args:   []any{},
+		},
+		{
+			filter: `has_incomplete_tasks == true`,
+			want:   "JSON_EXTRACT(`memo`.`payload`, '$.property.hasIncompleteTasks') = CAST('true' AS JSON)",
+			args:   []any{},
+		},
+		{
+			filter: `has_incomplete_tasks != false`,
+			want:   "JSON_EXTRACT(`memo`.`payload`, '$.property.hasIncompleteTasks') != CAST('false' AS JSON)",
+			args:   []any{},
+		},
+		{
+			filter: `has_incomplete_tasks == false`,
+			want:   "JSON_EXTRACT(`memo`.`payload`, '$.property.hasIncompleteTasks') = CAST('false' AS JSON)",
+			args:   []any{},
+		},
+		{
+			filter: `!has_incomplete_tasks`,
+			want:   "NOT (JSON_EXTRACT(`memo`.`payload`, '$.property.hasIncompleteTasks') = CAST('true' AS JSON))",
+			args:   []any{},
+		},
+		{
+			filter: `has_incomplete_tasks && pinned`,
+			want:   "(JSON_EXTRACT(`memo`.`payload`, '$.property.hasIncompleteTasks') = CAST('true' AS JSON) AND `memo`.`pinned` IS TRUE)",
+			args:   []any{},
+		},
+		{
+			filter: `has_incomplete_tasks && content.contains("todo")`,
+			want:   "(JSON_EXTRACT(`memo`.`payload`, '$.property.hasIncompleteTasks') = CAST('true' AS JSON) AND `memo`.`content` LIKE ?)",
+			args:   []any{"%todo%"},
+		},
 	}
 
 	for _, tt := range tests {
