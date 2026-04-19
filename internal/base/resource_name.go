bbded584ce85a856d863485768e08b53adec7244
George Wu <gbw@users.noreply.github.com>
2026-04-18 22:05:08 -0700

    fix: user resource names can be uuidv4 from idp sub claim (#5856)

--- internal/base/resource_name.go
+++ internal/base/resource_name.go
@@ -3,5 +3,5 @@ package base
 import "regexp"
 
 var (
-	UIDMatcher = regexp.MustCompile("^[a-zA-Z0-9]([a-zA-Z0-9-]{0,30}[a-zA-Z0-9])?$")
+	UIDMatcher = regexp.MustCompile("^[a-zA-Z0-9]([a-zA-Z0-9-]{0,34}[a-zA-Z0-9])?$")
 )
