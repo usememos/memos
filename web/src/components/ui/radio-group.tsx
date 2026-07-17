2d01420c23372d5708904ec78e03485edb44d49b
Dedy F. Setyawan <dedyfajars@gmail.com>
2026-07-18 00:47:45 +0700

    fix(web): align radio button group indicator (#6097)
    Signed-off-by: Dedy F. Setyawan <dedyfajars@gmail.com>

--- web/src/components/ui/radio-group.tsx
+++ web/src/components/ui/radio-group.tsx
@@ -17,7 +17,7 @@ function RadioGroupItem({ className, ...props }: Radio.Root.Props) {
       )}
       {...props}
     >
-      <Radio.Indicator data-slot="radio-group-indicator" className="relative flex items-center justify-center">
+      <Radio.Indicator data-slot="radio-group-indicator" className="relative flex size-full items-center justify-center">
         <CircleIcon className="fill-primary absolute top-1/2 left-1/2 size-2 -translate-x-1/2 -translate-y-1/2" />
       </Radio.Indicator>
     </Radio.Root>
